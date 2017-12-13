777ff2f8976644b34124111752a5d369c4639202
danfbflynn <Daniel.Flynn.CTR@ad.dot.gov>
2017-12-13 09:43:46 -0500

    New aggregation for daily files
    Currently running on April

--- ReduceWaze2.R
+++ ReduceWaze2.R
@@ -7,13 +7,15 @@
 
 library(tidyverse)
 
-mappeddrive = "W:" # change this to match the mapped network drive on your machine (Z: in original)
+if(length(grep("flynn", getwd())) > 0) {mappeddrive = "W:"} # change this to match the mapped network drive on your machine (Z: in original)
+if(length(grep("sudderth", getwd())) > 0) {mappeddrive = "S:"} 
 
 setwd(file.path(mappeddrive,"SDI Pilot Projects/Waze/MASTER Data Files/Waze"))
 
 # Helper functions ----
 
-# Function to return the most frequent value for character vectors
+# Function to return the most frequent value for character vectors.
+# Breaks ties by taking the first value
 ModeString <- function(x) {
   ux <- unique(x)
   
@@ -41,7 +43,23 @@ gettime <- function(x, tz = "America/New_York"){
   dt <- strptime(paste(d, t), "%Y-%m-%d %H-%M-%S", tz = tz)
 }
 
+# moving files from a temporary directory on local machine to shared drive. 
+# Files are removed from the local machine by this process.
+movefiles <- function(filelist, temp = outdir, wazedir){
+  for(i in filelist){
+      # Fix path separators for Windows / R 
+      temp <- gsub("\\\\", "/", temp)
+      temp <- gsub("C:/U", "C://U", temp)
+      
+      # Encase the destination path in quotes, because of spaces in path name
+      system(paste0("move ", file.path(temp, i), " '", file.path(wazedir, i), "'"))
+    
+      }
+    }
+
+# <><><><><><><><><><><><><><><><><><><><><><><><>
 # Make aggregated daily files ----
+# <><><><><><><><><><><><><><><><><><><><><><><><>
 
 # read file names
 wazefiles <- dir()[grep("csv", dir())]
@@ -49,8 +67,11 @@ wazefiles <- dir()[grep("csv", dir())]
 # For now: April only
 wazefiles = wazefiles[substr(wazefiles, 10, 11) == "04"]
 
+# create a temporary directory on the C: drive of the local machine to save files
+outdir <- tempdir()
+
 #for each file
-j = 1 # start at 21 currently...
+j = 1 # 
 
 starttime <- Sys.time()
 
@@ -66,20 +87,15 @@ while (j <= length(wazefiles)){
   bydate <- bydate[is.na(match(names(bydate), dropcols))]
   
  
-  #aggregate by uuid. Now using dplyr 
+  #aggregate by uuid. Now using dplyr.
+  # First step: median values for report rating, confidence, and reliability. Earliest report rating as min(pubMillis). Lat long as median of all reported values. File names for first and last pull date. Sum of records 
   d <- as.tibble(bydate)
   ll <- d %>%
          group_by(uuid) %>%
          summarise(
-                   city = ModeString(city),
-                   reportRating = median(reportRating),
-                   confidence = median(confidence),
-                   reliability = median(reliability),
-                   type = ModeString(type),
-                   roadType = ModeString(as.character(roadType)),
-                   magvar = max(magvar),
-                   subtype = ModeString(subtype),
-                   street = ModeString(street),
+                   median.reportRating = median(reportRating),
+                   median.confidence = median(confidence),
+                   median.reliability = median(reliability),
                    pubMillis = min(pubMillis),
                    lon = median(location.x),
                    lat = median(location.y),
@@ -87,7 +103,27 @@ while (j <= length(wazefiles)){
                    last.file = sort(filename, decreasing = T)[1],
                    nrecord = n()
                    )
+    # now add the following for the *most recent* row within a uuid, using filter(date == max(date)): city, report rating, confidence, and reliability, type, road type, directoin as max(magvar), subtype and street.
+    # Using ModeString function here in case there are multiple uuid at the maximum date (should not be)
+    ll2 <-  d %>% 
+        group_by(uuid) %>%
+        filter(date == max(date)) %>%
+        summarize(
+          city = ModeString(city),
+          last.reportRating = median(reportRating),
+          last.confidence = median(confidence),
+          last.reliability = median(reliability),
+          type = ModeString(type),
+          roadType = ModeString(as.character(roadType)),
+          magvar = max(magvar),
+          subtype = ModeString(subtype),
+          street = ModeString(street)
+          )
   
+    # join these two sets and sort columns 
+    lx <- full_join(ll, ll2, by = 'uuid')
+    ll <- lx # keep naming convention for compatibility with existing code
+    
   # inspect a particular uuid in original to test:
   # bydate[bydate$uuid=="6407a977-9d52-380e-b545-b4ef58e70337",]
   # test: xx <- tapply(bydate$roadType, bydate$uuid, ModeString); which(sapply(xx, length) > 1)
@@ -96,13 +132,8 @@ while (j <= length(wazefiles)){
   filenamesave <- sub("csv", "RData", wazefiles[j])
   
   save(list = "ll", 
-       file = file.path(mappeddrive,"/SDI Pilot Projects/Waze/MASTER Data Files/Waze Aggregated/day_MD", filenamesave))
-  
-  # write to csv temporarily as example
-  # write.csv(ll, 
-  #      file = file.path(mappeddrive,"/SDI Pilot Projects/Waze/MASTER Data Files/Waze Aggregated/day_MD", wazefiles[j]),
-  #      row.names = F)
-  
+       file = file.path(outdir, filenamesave))
+
   timediff <- round(Sys.time()-starttime, 2)
   cat(j, "complete \n", timediff, attr(timediff, "units"), "elapsed \n", rep("<>",20), "\n")
 
@@ -110,17 +141,27 @@ while (j <= length(wazefiles)){
   }
 
 
+# after completed, move these aggregated files from C: to shared drive
+wazedir <- file.path(mappeddrive,"/SDI Pilot Projects/Waze/MASTER Data Files/Waze Aggregated/day_MD")
+filelist <- dir(outdir)[grep("RData$", dir(outdir))]
+
+movefiles(filelist, outdir, wazedir)
+
+
+# <><><><><><><><><><><><><><><><><><><><><><><><>
 # Compiling Monthly ----
+# <><><><><><><><><><><><><><><><><><><><><><><><>
+
 
 setwd(file.path(mappeddrive, "/SDI Pilot Projects/Waze/MASTER Data Files/Waze Aggregated/day_MD"))
 
-#read files
+# read files
 wazefiles <- dir()
 
 # filter: only .RData
 wazefiles = wazefiles[grep("RData$", wazefiles)]
 
-# find available months (only April currently)
+# find available months (only April initially)
 months = unique(substr(wazefiles, 10, 11))
 
 # Read in each day, rbind together, 
