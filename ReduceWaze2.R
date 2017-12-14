353fbc2c8cb12afecd576e2504d948789ebb3aa5
danfbflynn <Daniel.Flynn.CTR@ad.dot.gov>
2017-12-14 09:14:23 -0500

    Running month aggregation

--- ReduceWaze2.R
+++ ReduceWaze2.R
@@ -153,8 +153,13 @@ movefiles(filelist, outdir, wazedir)
 # Compiling Monthly ----
 # <><><><><><><><><><><><><><><><><><><><><><><><>
 
+# Compiling daily into monthly files takes ~ 16 minutes per month
+# Consider speeding up with library(foreach), using %dopar%
 
 setwd(file.path(mappeddrive, "/SDI Pilot Projects/Waze/MASTER Data Files/Waze Aggregated/day_MD"))
+
+# create a temporary directory on the C: drive of the local machine to save files
+outdir <- tempdir()
 
 # read files
 wazefiles <- dir()
@@ -162,8 +167,11 @@ wazefiles <- dir()
 # filter: only .RData
 wazefiles = wazefiles[grep("RData$", wazefiles)]
 
-# find available months (only April initially)
+# find available months (was only April initially)
 months = unique(substr(wazefiles, 10, 11))
+
+starttime <- Sys.time()
+
 
 # Read in each day, rbind together, 
 # get dates in correct format, output to csv and RData
@@ -178,22 +186,21 @@ for(i in months){ # i = "04"
     load(wf[j]) # each RData has a dataframe ll for that day
     monthbind <- rbind(monthbind, ll)
     cat(". ")
-    }
+  }
+  
+  monthbind$date <- as.POSIXct(paste(
+    substr(monthbind$last.file, 5, 14), " ", substr(monthbind$last.file, 16, 17), ":", 
+    substr(monthbind$last.file, 19,20), ":", substr(monthbind$last.file, 22, 23), sep = ""))
+  
 
   # Aggregate again (uuid can span days). This can be very slow. 
   d <- as.tibble(monthbind)
   ll <- d %>%
     group_by(uuid) %>%
     summarise(
-      city = ModeString(city),
-      reportRating = median(reportRating),
-      confidence = median(confidence),
-      reliability = median(reliability),
-      type = ModeString(type),
-      roadType = ModeString(as.character(roadType)),
-      magvar = max(magvar),
-      subtype = ModeString(subtype),
-      street = ModeString(street),
+      median.reportRating = median(median.reportRating),
+      median.confidence = median(median.confidence),
+      median.reliability = median(median.reliability),
       pubMillis = min(pubMillis),
       lon = median(lon),
       lat = median(lat),
@@ -201,6 +208,27 @@ for(i in months){ # i = "04"
       last.file = sort(last.file, decreasing = T)[1],
       nrecord = sum(nrecord) # instead of n(), because now aggregating the aggregated version 
     )
+  # now add the following for the *most recent* row within a uuid, using filter(date == max(date)): city, report rating, confidence, and reliability, type, road type, directoin as max(magvar), subtype and street.
+  # Using ModeString function here in case there are multiple uuid at the maximum date (should not be)
+  ll2 <-  d %>% 
+    group_by(uuid) %>%
+    filter(date == max(pubMillis)) %>%
+    summarize(
+      city = ModeString(city),
+      last.reportRating = median(last.reportRating),
+      last.confidence = median(last.confidence),
+      last.reliability = median(last.reliability),
+      type = ModeString(type),
+      roadType = ModeString(as.character(roadType)),
+      magvar = max(magvar),
+      subtype = ModeString(subtype),
+      street = ModeString(street)
+    )
+  
+  # join these two sets and sort columns 
+  lx <- full_join(ll, ll2, by = 'uuid')
+  ll <- lx # keep naming convention for compatibility with existing code
+  
   
   # Format and output  
   time <- as.POSIXct(ll$pubMillis/1000, origin = "1970-01-01", tz="America/New_York") # Time zone will need to be correctly configured for other States.
@@ -218,10 +246,22 @@ for(i in months){ # i = "04"
                    sep = "")
   
   write.csv(mb,
-            file = paste0("../month_MD/", filenamesave, ".csv"),
+            file = file.path(outdir, paste0(filenamesave, ".csv")),
             row.names = F)
   
   save(list = "mb", 
-       file = paste0("../month_MD/", filenamesave, ".RData"))
+       file = file.path(outdir, paste0(filenamesave, ".RData")))
+
+  timediff <- round(Sys.time()-starttime, 2)
+  cat(j, "complete \n", timediff, attr(timediff, "units"), "elapsed \n", rep("<>",20), "\n")
   
-  }
+  
+  } # end month bind loop
+
+# after completed, move these aggregated files from C: to shared drive
+wazedir <- file.path(mappeddrive,"/SDI Pilot Projects/Waze/MASTER Data Files/Waze Aggregated/month_MD")
+
+# Move either RData or csv 
+filelist <- dir(outdir)[grep("[RData$csv$]", dir(outdir))]
+
+movefiles(filelist, outdir, wazedir)
