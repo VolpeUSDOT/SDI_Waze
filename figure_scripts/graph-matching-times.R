bb88257c4b009e69cef358e474aa41e2e0ea613c
danfbflynn <Daniel.Flynn.CTR@ad.dot.gov>
2017-12-21 14:12:45 -0500

    Difference panel for matching times plot

--- figure_scripts/graph-matching-times.R
+++ figure_scripts/graph-matching-times.R
@@ -3,16 +3,28 @@
 
 # Started by Stephen Brumbaugh @sbrumb
 
+library(extrafont)
+extrafont::loadfonts(device="win") # needed on Windows machines
+# extrafont::font_import() # will import all fonts, takes time
+
 library(tidyverse)
 library(lubridate)
 library(haven)
 library(forcats)
 library(scales)
-library(extrafont)
 library(Cairo)
+library(gridExtra) # for grid.arrange
 
-match <- read_sas('../../SAS/uniqueaccidentsmerge.sas7bdat')
-edt <- read_sas('../../SAS/mdcrashfact.sas7bdat')
+# Code location
+mappeddriveloc <- "W:"
+
+codeloc <- "~/git/SDI_Waze"
+wazedir <- file.path(mappeddriveloc, "SDI Pilot Projects/Waze/Working Documents")
+
+setwd(wazedir)
+
+match <- read_sas('SAS/uniqueaccidentsmerge.sas7bdat')
+edt <- read_sas('SAS/mdcrashfact.sas7bdat')
 
 # Summarize EDT data
 summary_edt <- edt %>%
@@ -52,7 +64,7 @@ theme_ppt <- theme_void() +
   )
 
 # Plot and save
-summary %>%
+p1 <- summary %>%
   ggplot(aes(x = hour)) +
   geom_bar(aes(y = freq, fill = work),
            stat = "identity", width = .8, show.legend = FALSE) +
@@ -63,5 +75,75 @@ summary %>%
              labeller = labeller(.multi_line = FALSE)) +
   theme_ppt
 
-ggsave("plots/waze reporting.png", type = "cairo-png",
+p1
+
+ggsave("R/Graphics/plots/waze reporting.png", type = "cairo-png",
        width = 9, height = 5)
+
+
+## Add a difference panel
+
+summary_difference <- summary_match
+summary_difference$freq <- summary_match$freq - summary_edt$freq
+summary_difference$source <- "zDifference"
+
+
+summary2 <- rbind(summary_match, summary_edt, summary_difference) %>%
+  mutate(work = factor(WorkWeek),
+         work = fct_recode(work, "Weekday" = "Work Week"))
+
+# Plot and save
+p2 <- summary2 %>%
+  ggplot(aes(x = hour)) +
+  geom_bar(aes(y = freq, fill = work),
+           stat = "identity", width = .8, show.legend = FALSE) +
+  scale_y_continuous(labels = percent,
+                     breaks = pretty_breaks(n = 5)) +
+  labs(x = "Hour of day", y = "Percentage") +
+  facet_wrap(~source + work,
+             nrow = 3, ncol = 2,
+             # scales = 'free',
+             labeller = labeller(.multi_line = FALSE)) +
+  theme_ppt
+
+p2 
+
+# Another idea: plot separately and use grid.arrange, make postitive and negative values have different colors, set y-axis limits for the differene plot smaller.
+
+summary_difference <- summary_match
+summary_difference$freq <- summary_match$freq - summary_edt$freq
+summary_difference$source <- "Difference"
+summary_difference$sign <- ifelse(summary_difference$freq >= 0, 'positive', 'negative')
+
+summary_difference <- summary_difference %>%
+  mutate(work = factor(WorkWeek),
+         work = fct_recode(work, "Weekday" = "Work Week"))
+
+p3 <- summary_difference %>%
+  ggplot(aes(x = hour)) +
+  geom_bar(aes(y = freq, fill = sign),
+           stat = "identity", width = .8, show.legend = FALSE) +
+  facet_wrap(~source + work,
+             labeller = labeller(.multi_line = FALSE)) +
+  scale_fill_manual(values = c("positive" = "midnightblue", "negative" = "firebrick3")) +
+  scale_y_continuous(labels = percent,
+                    breaks = pretty_breaks(n = 3)) +
+  labs(x = "Hour of day", y = "") +
+  theme_ppt
+p3
+
+
+# grid.arragne to show, arrangeGrob to make new plot object
+grid.arrange(p1, p3,
+             nrow = 2,
+             heights = c(2.5, 1))
+
+p4 <- arrangeGrob(p1, p3,
+            nrow = 2,
+            heights = c(2.5, 1))
+
+ggsave("R/Graphics/plots/waze_reporting_difference.png", 
+       plot = p4,
+       type = "cairo-png",
+       width = 9, height = 5)
+system("open R/Graphics/plots/waze_reporting_difference.png")
