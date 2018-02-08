a13d97d8af78bc5167484d9436e4d33254cad632
dflynn-volpe <daniel.flynn.ctr@dot.gov>
2018-02-08 17:25:36 -0500

    Correct download mode for JDBC driver

--- utility/connect_redshift.R
+++ utility/connect_redshift.R
@@ -4,20 +4,20 @@
 # install.packages("RJDBC", dep = T)
 library(RJDBC)
 
-# Need to do this onely once. Download to a hidden folder for cleaner workspace. Latest in 2018-02 is 1.2.10.1009.
+# Look in .redshiftTools. If empty, download the driver from Amazon.
+# Download to a hidden folder for cleaner workspace. Latest in 2018-02 is 1.2.10.1009.
+# make sure to use mode = "wb" on Windows OS: "Since Windows (unlike Unix-alikes) does distinguish between text and binary files, care is needed that other binary file types are transferred with mode = "wb"."
 
 if(length(dir("~/.redshiftTools"))==0){
     system('mkdir ~/.redshiftTools')
-    download.file('https://s3.amazonaws.com/redshift-downloads/drivers/RedshiftJDBC42-1.2.10.1009.jar', '~/.redshiftTools/RedshiftJDBC42-1.2.10.1009.jar')
+    download.file('https://s3.amazonaws.com/redshift-downloads/drivers/RedshiftJDBC42-1.2.10.1009.jar', '~/.redshiftTools/RedshiftJDBC42-1.2.10.1009.jar', mode = "wb")
 }
 
 driver <- JDBC(driverClass = "com.amazon.redshift.jdbc42.Driver", 
                classPath = "~/.redshiftTools/RedshiftJDBC42-1.2.10.1009.jar",
                identifier.quote="`")
 
-# .jclassLoader()$setDebug(1L) # Set this to get more details if recieving 'class not found' error. Same error on ec2 instance inside SDC.
-# possible solutions: https://stackoverflow.com/questions/30738974/rjava-load-error-in-rstudio-r-after-upgrading-to-osx-yosemite
-# issue with Java class path? Iss
+# .jclassLoader()$setDebug(1L) # Set this to get more details if recieving 'class not found' error. Same error on ec2 instance inside SDC until changing download mode.
 
 # Specify username and password manually, once:
 if(Sys.getenv("sdc_waze_username")==""){
@@ -28,11 +28,3 @@ url <- sprintf("jdbc:redshift://prod-dot-sdc-redshift-cluster.cctxatvt4w6t.us-ea
                Sys.getenv("atadev_username"), Sys.getenv("atadev_password"))
 
 conn <- dbConnect(driver, url)
-
-
-dbGetQuery(conn, "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
-
-# For FARS, will need to edit
-dbGetQuery(conn, "SELECT day_week, sum(drunk_dr) from fars_accident_csv group by day_week order by 2 desc")
-
-dbDisconnect(conn)
