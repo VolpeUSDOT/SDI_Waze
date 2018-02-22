300c936c7e21f90035bf8b782f3cd25eb8e11dfa
dflynn-volpe <daniel.flynn.ctr@dot.gov>
2018-02-22 16:45:46 -0500

    Paralle Random Forest running
    Also updated diagnositc function for consistency with other references,
    observed as columns and predicted as rows.

--- analysis/RandomForest_WazeGrid.R
+++ analysis/RandomForest_WazeGrid.R
@@ -12,7 +12,9 @@ library(randomForest)
 library(maptree) # for better graphing
 library(party)
 library(partykit)
-library(rgdal) # for readOGR(), needed for reading in ArcM shapefiles
+library(foreach) # for parallel implementation
+library(doParallel) # includes iterators and parallel
+
 
 # Run this if you don't have these packages:
 # install.packages(c("rpart", "randomForest", "maptree", "party", "partykit", "rgdal"), dep = T)
@@ -36,11 +38,11 @@ load("WazeEdtHex_Beta.RData")
 waze.edt.hex$DayOfWeek <- as.factor(waze.edt.hex$DayOfWeek)
 waze.edt.hex$hour <- as.numeric(waze.edt.hex$hour)
 
-summary(waze.edt.hex$nMatchEDT_buffer)
-summary(waze.edt.hex$nMatchEDT_buffer > 1)
-table(fitdat.w$nMatchEDT_buffer[fitdat.w$nMatchEDT_buffer > 1])
 
 # Response variable: use continuous? Only 1,000 out of 238,000 cells have > 1 EDT event matching. Consider converting to binary. Of the >1 cell, 886 are 2 events, 122 3 events, tiny number have greater.
+summary(waze.edt.hex$nMatchEDT_buffer)
+summary(waze.edt.hex$nMatchEDT_buffer > 1)
+table(waze.edt.hex$nMatchEDT_buffer[waze.edt.hex$nMatchEDT_buffer > 1])
 
 # Going to binary:
 waze.edt.hex$MatchEDT_buffer <- waze.edt.hex$nMatchEDT_buffer
@@ -49,10 +51,15 @@ waze.edt.hex$MatchEDT_buffer <- as.factor(waze.edt.hex$MatchEDT_buffer)
 
 # Analysis ----
 # Prepare data and formulas
-
 fitdat.w <- waze.edt.hex
+
+# Variables to test. Use Waze only predictors, and omit grid ID and day as predictors as well
+
 fitvars.w <- names(waze.edt.hex)[is.na(match(names(waze.edt.hex),
-                                             c("GRID_ID", "day", "uniqEDTreports")))]
+                                             c("GRID_ID", "day", # place variables to omit as predictors in this vector 
+                                               "nMatchWaze_buffer", "nNoMatchWaze_buffer",
+                                               grep("EDT", names(waze.edt.hex), value = T)
+                                               )))]
 
 fitdat.w <- fitdat.w[complete.cases(fitdat.w[,fitvars.w]),]
 
@@ -61,28 +68,101 @@ wazeformula <- reformulate(termlabels = fitvars.w[is.na(match(fitvars.w,
                            response = "MatchEDT_buffer")
 
 # <><><><><><><><><><><><><><><><><><><><><><><><><><><>
-# Conditional random forest from ctree ----
+# Random forest sequential ----
 # CPU intensive, but not RAM intensive. doMC should help.
 showobjsize() # from wazefunctions.R
 
+(avail.cores <- parallel::detectCores()) # 4 on local
+ntree.use = avail.cores * 100 
 
-starttime <- Sys.time()
+trainrows <- sort(sample(1:nrow(fitdat.w), size = nrow(fitdat.w)*.7, replace = F))
+testrows <- (1:nrow(fitdat.w))[!1:nrow(fitdat.w) %in% trainrows]
 
-rf.w <- randomForest(wazeformula,
-                     data = fitdat.w,
-                     ntree=500, nodesize=5, mtry=9)
+# length(testrows) + length(trainrows) == nrow(fitdat.w)
 
-rf.w.pred <- predict(rf.w, fitdat.w[fitvars.w[is.na(match(fitvars.w,
-                                                          "MatchEDT_buffer"))]])
+system.time(rf.w <- randomForest(wazeformula,
+                     data = fitdat.w[trainrows,],
+                     ntree = ntree.use,
+                     nodesize = 5,
+                     mtry = 9)
+            )
 
-table(fitdat.w$MatchEDT_buffer, rf.w.pred)
+# predict on test values
+system.time(rf.w.pred <- predict(rf.w, fitdat.w[testrows, fitvars.w]))
 
-cf.w = partykit::cforest(wazeformula,
-             data = fitdat.w[1:1000,],
-             trace = T,
-             ntree = 250)
+(predtab <- table(fitdat.w$MatchEDT_buffer[testrows], rf.w.pred))
+bin.mod.diagnostics(predtab)
 
-cat(Sys.time() - starttime, " elapsed for Waze")
+# rf.w.pred
+#       0     1
+# 0 67746   369
+# 1  2850   690
+# accuracy            0.9551
+# precision           0.1949
+# recall              0.6516
+# false.positive.rate 0.0404
+
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# Random forest parallel ----
+cl <- makeCluster(parallel::detectCores()) # make a cluster of all available cores
+registerDoParallel(cl)
+
+# On small job, user time faster but system time greater. On a large job, almost exactly ncore X faster!
+
+system.time(rf.p <- foreach(ntree = c(ntree.use/avail.cores, avail.cores),
+                .combine = combine,
+                .packages = "randomForest") %dopar%
+          randomForest(wazeformula,
+               data = fitdat.w[trainrows,],
+               ntree = ntree,
+               nodesize = 5,
+               mtry = 9)
+  )
+
+
+# prediction not as simple to parallelize... but very fast anyways
+# foreach(ntree = c(ntree.use/avail.cores, avail.cores),
+#         .combine = combine,
+#         .packages = "randomForest",
+#         #suba = isplitRows(a, chunkSize=10) # check split rows in iterators?
+#         ) %dopar% {
+#   predict(rf.p, newdata = fitdat.w[fitvars.w])
+# }
+
+system.time(rf.p.pred <- predict(rf.p, fitdat.w[testrows,fitvars.w]))
+
+(predtab <- table(fitdat.w$MatchEDT_buffer[testrows], rf.p.pred))
+bin.mod.diagnostics(predtab)
+
+# Nearly identical predictions and diagnostics. 
+# Accuracy: sum of true positives and true negatives, as a portion of total sample.
+# Precision: True positives as a portion of all predictions
+# Recall: true postives as a portion of all observed positives
+# False positive rate: False positives a portion of all observed negatives
+
+# rf.p.pred
+# Observed as columns, predicted as rows.
+#       0     1
+# 0 67740   375
+# 1  2846   694
+
+# accuracy            0.9550
+# precision           0.1960
+# recall              0.6492
+# false.positive.rate 0.0403
+
+stopCluster(cl) # stop the cluster when done
+
+
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# Conditional random forest from ctree ----
+
+# cf.w = partykit::cforest(wazeformula,
+#              data = fitdat.w[1:1000,],
+#              trace = T,
+#              ntree = 250)
+
+# cat(Sys.time() - starttime, " elapsed for Waze")
 # ~ 5 min for accident type only using party
 
 format(object.size(cf.w), units = "Mb")
