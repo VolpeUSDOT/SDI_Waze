0ebb5956533ff549452201a812b9516cddfc4415
dflynn-volpe <daniel.flynn.ctr@dot.gov>
2018-02-26 10:17:09 -0500

    Running April - June random forests
    With output summaries

--- analysis/RandomForest_WazeGrid.R
+++ analysis/RandomForest_WazeGrid.R
@@ -2,16 +2,25 @@
 
 # Goals: produce random forest analysis for features of Waze accidents and other event types that predict matching with EDT reports 
 
-# Running on gridded data
+# Running on gridded data, with Waze predictors only. Try three versions. For each one, produce confusion matrix, binary model diagnostics, and output .csv of grid IDs and four columns, one each for true negative, false postitive, false negative, true positive.
+
+# 1. April, 70/30 split
+# 2. April + May, 70 / 30 split
+# 3. April + May, predict June.
+
+# After this is complete, repeat these three with Waze accidents and nMatchEDT_acc as response only
+# If only Waze accident report
+# 1. Predict to nMatchEDT_buffer_acc
+# 2. Subset to only nWazeAccident > 1 ? look at this. 
 
 # <><><><><><><><><><><><><><><><><><><><>
 # Setup ----
 
-library(rpart)
+#library(rpart)
 library(randomForest)
 library(maptree) # for better graphing
-library(party)
-library(partykit)
+#library(party)
+#library(partykit)
 library(foreach) # for parallel implementation
 library(doParallel) # includes iterators and parallel
 
@@ -33,110 +42,251 @@ setwd(wazedir)
 # read utility functions, including movefiles
 source(file.path(codeloc, 'utility/wazefunctions.R'))
 
-load("WazeEdtHex_Beta.RData")
+# Read in data, renaming data files by month. For each month, prep time and response variables
 
-waze.edt.hex <- wazeTime.edt.hex
-
-waze.edt.hex$DayOfWeek <- as.factor(waze.edt.hex$DayOfWeek)
-waze.edt.hex$hour <- as.numeric(waze.edt.hex$hour)
-
-
-# Response variable: use continuous? Only 1,600 out of 310,000 cells have > 1 EDT event matching. Consider converting to binary. Of the >1 cell, 886 are 2 events, 122 3 events, tiny number have greater. 15,000 have 1.
-summary(waze.edt.hex$nMatchEDT_buffer)
-summary(waze.edt.hex$nMatchEDT_buffer > 1)
-table(waze.edt.hex$nMatchEDT_buffer[waze.edt.hex$nMatchEDT_buffer > 1])
-
+load("WazeTimeEdtHex_04.RData")
+wazeTime.edt.hex$DayOfWeek <- as.factor(wazeTime.edt.hex$DayOfWeek)
+wazeTime.edt.hex$hour <- as.numeric(wazeTime.edt.hex$hour)
 # Going to binary:
-waze.edt.hex$MatchEDT_buffer <- waze.edt.hex$nMatchEDT_buffer
-waze.edt.hex$MatchEDT_buffer[waze.edt.hex$MatchEDT_buffer > 0] = 1 
-waze.edt.hex$MatchEDT_buffer <- as.factor(waze.edt.hex$MatchEDT_buffer)
+wazeTime.edt.hex$MatchEDT_buffer <- wazeTime.edt.hex$nMatchEDT_buffer
+wazeTime.edt.hex$MatchEDT_buffer[wazeTime.edt.hex$MatchEDT_buffer > 0] = 1 
+wazeTime.edt.hex$MatchEDT_buffer <- as.factor(wazeTime.edt.hex$MatchEDT_buffer)
+
+w.04 <- wazeTime.edt.hex; rm(wazeTime.edt.hex) 
+
+load("WazeTimeEdtHex_05.RData")
+wazeTime.edt.hex$DayOfWeek <- as.factor(wazeTime.edt.hex$DayOfWeek)
+wazeTime.edt.hex$hour <- as.numeric(wazeTime.edt.hex$hour)
+wazeTime.edt.hex$MatchEDT_buffer <- wazeTime.edt.hex$nMatchEDT_buffer
+wazeTime.edt.hex$MatchEDT_buffer[wazeTime.edt.hex$MatchEDT_buffer > 0] = 1 
+wazeTime.edt.hex$MatchEDT_buffer <- as.factor(wazeTime.edt.hex$MatchEDT_buffer)
+
+w.05 <- wazeTime.edt.hex; rm(wazeTime.edt.hex) 
+
+load("WazeTimeEdtHex_06.RData")
+wazeTime.edt.hex$DayOfWeek <- as.factor(wazeTime.edt.hex$DayOfWeek)
+wazeTime.edt.hex$hour <- as.numeric(wazeTime.edt.hex$hour)
+wazeTime.edt.hex$MatchEDT_buffer <- wazeTime.edt.hex$nMatchEDT_buffer
+wazeTime.edt.hex$MatchEDT_buffer[wazeTime.edt.hex$MatchEDT_buffer > 0] = 1 
+wazeTime.edt.hex$MatchEDT_buffer <- as.factor(wazeTime.edt.hex$MatchEDT_buffer)
+
+w.06 <- wazeTime.edt.hex; rm(wazeTime.edt.hex) 
+
+
+# Exploration of response variable: For April, only 1,600 out of 310,000 cells have > 1 EDT event matching. Consider converting to binary. Of the >1 cell, 886 are 2 events, 122 3 events, tiny number have greater. 15,000 have 1.
+# summary(w.05$nMatchEDT_buffer > 1)
+# table(w.05$nMatchEDT_buffer[w.05$nMatchEDT_buffer > 1])
+
 
 # Analysis ----
-# Prepare data and formulas
-fitdat.w <- waze.edt.hex
 
 # Variables to test. Use Waze only predictors, and omit grid ID and day as predictors as well
 
-fitvars.w <- names(waze.edt.hex)[is.na(match(names(waze.edt.hex),
+fitvars <- names(w.04)[is.na(match(names(w.04),
                                              c("GRID_ID", "day", # place variables to omit as predictors in this vector 
                                                "nMatchWaze_buffer", "nNoMatchWaze_buffer",
-                                               grep("EDT", names(waze.edt.hex), value = T)
+                                               grep("EDT", names(w.04), value = T)
                                                )))]
 
-fitdat.w <- fitdat.w[complete.cases(fitdat.w[,fitvars.w]),]
+# Unnecessary now: all rows are complete cases
+# fitdat.04 <- w.04[complete.cases(w.04[,fitvars]),]
+# fitdat.05 <- w.05[complete.cases(w.05[,fitvars]),]
+# fitdat.06 <- w.06[complete.cases(w.06[,fitvars]),]
 
-wazeformula <- reformulate(termlabels = fitvars.w[is.na(match(fitvars.w,
+wazeformula <- reformulate(termlabels = fitvars[is.na(match(fitvars,
                                                               "MatchEDT_buffer"))], 
                            response = "MatchEDT_buffer")
 
-# <><><><><><><><><><><><><><><><><><><><><><><><><><><>
-# Random forest sequential ----
-# CPU intensive, but not RAM intensive. doMC should help.
-showobjsize() # from wazefunctions.R
-
-(avail.cores <- parallel::detectCores()) # 4 on local
-ntree.use = avail.cores * 100 
-
-trainrows <- sort(sample(1:nrow(fitdat.w), size = nrow(fitdat.w)*.7, replace = F))
-testrows <- (1:nrow(fitdat.w))[!1:nrow(fitdat.w) %in% trainrows]
-
-# length(testrows) + length(trainrows) == nrow(fitdat.w)
-
-system.time(rf.w <- randomForest(wazeformula,
-                     data = fitdat.w[trainrows,],
-                     ntree = ntree.use,
-                     nodesize = 5,
-                     mtry = 9)
-            )
-
-# predict on test values
-system.time(rf.w.pred <- predict(rf.w, fitdat.w[testrows, fitvars.w]))
-
-(predtab <- table(fitdat.w$MatchEDT_buffer[testrows], rf.w.pred))
-bin.mod.diagnostics(predtab)
-
-# rf.w.pred
-#       0     1
-# 0 67746   369
-# 1  2850   690
-# accuracy            0.9551
-# precision           0.1949
-# recall              0.6516
-# false.positive.rate 0.0404
 
 # <><><><><><><><><><><><><><><><><><><><><><><><><><><>
 # Random forest parallel ----
 cl <- makeCluster(parallel::detectCores()) # make a cluster of all available cores
 registerDoParallel(cl)
 
-# On small job, user time faster but system time greater. On a large job, almost exactly ncore X faster!
+# On small job, user time faster but system time greater. On a large job, almost exactly ncore X faster
+(avail.cores <- parallel::detectCores()) # 4 on local
+ntree.use = avail.cores * 100 
 
-system.time(rf.p <- foreach(ntree = c(ntree.use/avail.cores, avail.cores),
+
+# Model 1: April, 70/30 ----
+# approx 2.5 min to run on 218k rows of training data with 4 cores
+
+trainrows <- sort(sample(1:nrow(w.04), size = nrow(w.04)*.7, replace = F))
+testrows <- (1:nrow(w.04))[!1:nrow(w.04) %in% trainrows]
+ 
+# length(testrows) + length(trainrows) == nrow(w.04)
+
+system.time(rf.04 <- foreach(ntree = c(ntree.use/avail.cores, avail.cores),
                 .combine = combine,
                 .packages = "randomForest") %dopar%
           randomForest(wazeformula,
-               data = fitdat.w[trainrows,],
+               data = w.04[trainrows,],
                ntree = ntree,
                nodesize = 5,
                mtry = 9)
   )
 
 
-# prediction not as simple to parallelize... but very fast anyways
-# foreach(ntree = c(ntree.use/avail.cores, avail.cores),
-#         .combine = combine,
-#         .packages = "randomForest",
-#         #suba = isplitRows(a, chunkSize=10) # check split rows in iterators?
-#         ) %dopar% {
-#   predict(rf.p, newdata = fitdat.w[fitvars.w])
-# }
+system.time(rf.04.pred <- predict(rf.04, w.04[testrows, fitvars]))
 
-system.time(rf.p.pred <- predict(rf.p, fitdat.w[testrows,fitvars.w]))
+Nobs <- data.frame(t(c(nrow(w.04),
+               summary(w.04$MatchEDT_buffer),
+               length(w.04$nWazeAccident[w.04$nWazeAccident>0])
+               )))
 
-(predtab <- table(fitdat.w$MatchEDT_buffer[testrows], rf.p.pred))
+colnames(Nobs) = c("N", "No EDT", "EDT present", "Waze accident present")
+format(Nobs, big.mark = ",")
+
+(predtab <- table(w.04$MatchEDT_buffer[testrows], rf.04.pred))
 bin.mod.diagnostics(predtab)
 
-# Nearly identical predictions and diagnostics. 
+# save output predictions
+
+out.04 <- data.frame(w.04$GRID_ID[testrows], w.04$MatchEDT_buffer[testrows], rf.04.pred)
+
+names(out.04) <- c("GRID_ID", "Obs", "Pred")
+
+out.04 = data.frame(out.04,
+                    TN = out.04$Obs == 0 &  out.04$Pred == 0,
+                    FP = out.04$Obs == 0 &  out.04$Pred == 1,
+                    FN = out.04$Obs == 1 &  out.04$Pred == 0,
+                    TP = out.04$Obs == 1 &  out.04$Pred == 1)
+write.csv(out.04,
+          file = "RandomForest_pred_04.csv",
+          row.names = F)
+                    
+varImpPlot(rf.04) # variable imporatance: mean decrease in Gini impurity for this predictor across all trees.  
+
+save(list = c("rf.04",
+              "rf.04.pred",
+              "testrows",
+              "trainrows",
+              "w.04",
+              "out.04"),
+     file = "RandomForest_Output_04.RData")
+
+# Model 2: April + May, 70/30 ----
+# ~ 7 min to run on 460k rows training data with 4 cores
+
+w.0405 <- rbind(w.04, w.05)
+
+trainrows <- sort(sample(1:nrow(w.0405), size = nrow(w.0405)*.7, replace = F))
+testrows <- (1:nrow(w.0405))[!1:nrow(w.0405) %in% trainrows]
+
+system.time(rf.0405 <- foreach(ntree = c(ntree.use/avail.cores, avail.cores),
+                             .combine = combine,
+                             .packages = "randomForest") %dopar%
+              randomForest(wazeformula,
+                           data = w.0405[trainrows,],
+                           ntree = ntree,
+                           nodesize = 5,
+                           mtry = 9)
+)
+
+
+system.time(rf.0405.pred <- predict(rf.0405, w.0405[testrows, fitvars]))
+
+Nobs <- data.frame(t(c(nrow(w.0405),
+                       summary(w.0405$MatchEDT_buffer),
+                       length(w.0405$nWazeAccident[w.0405$nWazeAccident>0])
+)))
+
+colnames(Nobs) = c("N", "No EDT", "EDT present", "Waze accident present")
+format(Nobs, big.mark = ",")
+
+(predtab <- table(w.0405$MatchEDT_buffer[testrows], rf.0405.pred))
+bin.mod.diagnostics(predtab)
+
+# save output predictions
+
+out.0405 <- data.frame(w.0405$GRID_ID[testrows], w.0405$MatchEDT_buffer[testrows], rf.0405.pred)
+
+names(out.0405) <- c("GRID_ID", "Obs", "Pred")
+
+out.0405 = data.frame(out.0405,
+                    TN = out.0405$Obs == 0 &  out.0405$Pred == 0,
+                    FP = out.0405$Obs == 0 &  out.0405$Pred == 1,
+                    FN = out.0405$Obs == 1 &  out.0405$Pred == 0,
+                    TP = out.0405$Obs == 1 &  out.0405$Pred == 1)
+write.csv(out.0405,
+          file = "RandomForest_pred_0405.csv",
+          row.names = F)
+
+save(list = c("rf.0405",
+              "rf.0405.pred",
+              "testrows",
+              "trainrows",
+              "w.0405",
+              "out.0405"),
+     file = "RandomForest_Output_0405.RData")
+
+varImpPlot(rf.0405) # variable importance plot
+
+# Model 3: April + May, predict June
+# ~ X min to run on 657k rows training data on 4 cores
+
+system.time(rf.0405.all <- foreach(ntree = c(ntree.use/avail.cores, avail.cores),
+                               .combine = combine,
+                               .packages = "randomForest") %dopar%
+              randomForest(wazeformula,
+                           data = w.0405,
+                           ntree = ntree,
+                           nodesize = 5,
+                           mtry = 9)
+)
+
+
+system.time(rf.0405.06.pred <- predict(rf.0405.all, w.06[, fitvars]))
+
+w.040506 <- rbind(w.0405, w.06)
+
+Nobs <- data.frame(t(c(nrow(w.040506),
+                       summary(w.040506$MatchEDT_buffer),
+                       length(w.040506$nWazeAccident[w.040506$nWazeAccident>0])
+)))
+
+colnames(Nobs) = c("N", "No EDT", "EDT present", "Waze accident present")
+format(Nobs, big.mark = ",")
+
+(predtab <- table(w.06$MatchEDT_buffer, rf.0405.06.pred))
+bin.mod.diagnostics(predtab)
+
+
+# save output predictions
+
+out.06 <- data.frame(w.06$GRID_ID, w.06$MatchEDT_buffer, rf.0405.06.pred)
+
+names(out.06) <- c("GRID_ID", "Obs", "Pred")
+
+out.06 = data.frame(out.06,
+                      TN = out.06$Obs == 0 &  out.06$Pred == 0,
+                      FP = out.06$Obs == 0 &  out.06$Pred == 1,
+                      FN = out.06$Obs == 1 &  out.06$Pred == 0,
+                      TP = out.06$Obs == 1 &  out.06$Pred == 1)
+write.csv(out.06,
+          file = "RandomForest_pred_0405_06.csv",
+          row.names = F)
+
+save(list = c("rf.0405.all",
+              "rf.0405.06.pred",
+              "w.06",
+              "out.06"),
+     file = "RandomForest_Output_0405_06.RData")
+
+varImpPlot(rf.0405.all) # variable importance plot
+
+
+
+
+
+
+
+
+stopCluster(cl) # stop the cluster when done
+
+# Scratch ----
+
+# Comparison of sequential and parallel: identical predictions and diagnostics. 
 # Accuracy: sum of true positives and true negatives, as a portion of total sample.
 # Precision: True positives as a portion of all predictions
 # Recall: true postives as a portion of all observed positives
@@ -153,12 +303,37 @@ bin.mod.diagnostics(predtab)
 # recall              0.6492
 # false.positive.rate 0.0403
 
-stopCluster(cl) # stop the cluster when done
 
 
-# If only Waze accident report
-# 1. Predict to nMatchEDT_buffer_acc
-# 2. Subset to only nWazeAccident > 1 ? look at this. 
+ 
+# # <><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# # Random forest sequential ----
+# # CPU intensive, but not RAM intensive. doMC should help.
+# showobjsize() # from wazefunctions.R
+# 
+# 
+# 
+# system.time(rf.w <- randomForest(wazeformula,
+#                                  data = fitdat.w[trainrows,],
+#                                  ntree = ntree.use,
+#                                  nodesize = 5,
+#                                  mtry = 9)
+# )
+# 
+# # predict on test values
+# system.time(rf.w.pred <- predict(rf.w, fitdat.w[testrows, fitvars.w]))
+# 
+# (predtab <- table(fitdat.w$MatchEDT_buffer[testrows], rf.w.pred))
+# bin.mod.diagnostics(predtab)
+# 
+# # rf.w.pred
+# #       0     1
+# # 0 67746   369
+# # 1  2850   690
+# # accuracy            0.9551
+# # precision           0.1949
+# # recall              0.6516
+# # false.positive.rate 0.0404
 
 # <><><><><><><><><><><><><><><><><><><><><><><><><><><>
 # Conditional random forest from ctree ----
