22a4c8aac4192a1bdefb1b1d0fcbd2b1b9d409e7
Dan Flynn <daniel.flynn.ctr@dot.gov>
2018-03-02 14:00:07 +0000

    Starting Redshift queries

--- utility/connect_redshift_pgsql.R
+++ utility/connect_redshift_pgsql.R
@@ -7,8 +7,11 @@ library(RPostgreSQL)
 
 # Specify username and password manually, once:
 if(Sys.getenv("sdc_waze_username")==""){
-  cat("Please enter SDC Waze username and password manually the first time accessing the Redshift database, using: \n Sys.setenv('sdc_waze_username' = <see email from SDC Administrator>) \n Sys.setenv('sdc_waze_password' = <see email from SDC Administrator>)")
+  cat("Please enter SDC Waze username and password manually, in the console, the first time accessing the Redshift database, using: \n Sys.setenv('sdc_waze_username' = <see email from SDC Administrator>) \n Sys.setenv('sdc_waze_password' = <see email from SDC Administrator>)")
+
 }
+
+
 
 
 redshift_host <- "prod-dot-sdc-redshift-cluster.cctxatvt4w6t.us-east-1.redshift.amazonaws.com"
@@ -35,4 +38,30 @@ conn <- dbConnect(
 # })
 # 
 # 
+
+alert_query <- "SELECT * FROM alert LIMIT 5"
+results <- dbGetQuery(conn, alert_query)
+
+alert_query_MD <- "SELECT * FROM alert 
+                    WHERE state='MD'
+                    LIMIT 5"
+results <- dbGetQuery(conn, alert_query_MD)
+# add in time
+
+# time functions: strftime and to_timestamp
+# WHERE pub_millis BETWEEN to_date('01-APR-17','DD-MON-YY') AND to_date('03-APR-17','DD-MON-YY')
+
+# add to end to limit the nrows: LIMIT 50000
+
+alert_query_MD <- "SELECT * FROM alert 
+                    WHERE state='MD' 
+                    AND pub_utc_timestamp BETWEEN to_timestamp('2017-04-01 00:00:00','YYYY-MM-DD HH24:MI:SS') AND to_timestamp('2017-04-30 23:59:59','YYYY-MM-DD HH24:MI:SS')
+                      "
+results <- dbGetQuery(conn, alert_query_MD)
+
+format(object.size(results), "Gb") # 1.3 Gb for April 2017 Md data,
+
+summary(duplicated(results$alert_uuid)) # Avg 8x duplicated uuids
+
+system('free -m') # 16 GB ram, 6 used 
 
