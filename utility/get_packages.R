9aa7b18a80ae88fdbff4242038264736b639f1aa
Dan Flynn <daniel.flynn.ctr@dot.gov>
2018-03-14 13:28:15 +0000

    GDAL issues in Waze_clip

---
+++ utility/get_packages.R
@@ -0,0 +1,23 @@
+# Get all necessary packages across data prep and analysis scripts for SDI Waze project
+# If this fails, see "Package Installation Notes.Rmd" for details on tidyverse installation
+
+loadpacks <- c("sp",
+           "maps",
+           "reshape",
+           "maptools",
+           "mapproj",  # for coord_map(", 
+           "rgdal",  # for readOGR(", , needed for reading in ArcM shapefiles
+           "rgeos",  # for gIntersection, to clip two shapefiles
+           "raster", 
+           "lubridate",
+           "utils",
+           "randomForest",
+           "maptree", # for better graphing of decision trees
+           "party",
+           "partykit",
+           "foreach", # for parallel implementation
+           "doParallel",
+           "tidyverse")
+
+
+for(i in loadpacks){if(length(grep(i, (.packages(all.available=T))))==0) install.packages(i, dependencies =TRUE)}
