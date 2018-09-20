a23284b19a6e9b66786747a65a0f002d32002f12
Dan Flynn <daniel.flynn.ctr@dot.gov>
2018-09-20 19:01:54 +0000

    Prep of data for Tableau and CT MD compare update

--- analysis/Data_prep_for_Tableau.R
+++ analysis/Data_prep_for_Tableau.R
@@ -1,190 +1,199 @@
 # Data prep for Tableau viz
 
 # setup ----
-library(aws.s3)
 library(tidyverse)
 
+# Setup ---- 
+rm(list=ls()) # Start fresh
+library(randomForest)
+library(foreach) # for parallel implementation
+library(doParallel) # includes iterators and parallel
+library(tidyverse)
+library(rgdal)
+
 codeloc <- "~/SDI_Waze" 
+source(file.path(codeloc, 'utility/get_packages.R')) # installs necessary packages
+
 # Set grid size:
-HEXSIZE = c("1", "4", "05")[1] # Change the value in the bracket to use 1, 4, or 0.5 sq mi hexagon grids
-do.months = c("04","05","06","07","08","09")
+HEXSIZE = 1 
 
-inputdir <- "WazeEDT_RData_Input"
-outputdir <- paste0("WazeEDT_Agg", HEXSIZE, "mile_RandForest_Output")
+# Manually setting months to run here; could also scan S3 for months available for this state
+do.months = paste("2017", c("04","05","06","07","08","09"), sep="-")
 
-aws.signature::use_credentials()
-waze.bucket <- "ata-waze"
-localdir <- "/home/dflynn-volpe/workingdata" 
+REASSESS = F # re-assess model fit and diagnostics using reassess.rf instead of do.rf
+
+teambucket <- "s3://prod-sdc-sdi-911061262852-us-east-1-bucket"
+
+user <- paste0( "/home/", system("whoami", intern = TRUE)) # the user directory to use
+localdir <- paste0(user, "/workingdata") # full path for readOGR
+
+outputdir <- file.path(localdir, "Random_Forest_Output")
 
 setwd(localdir)
-# read utility functions, including prep.hex and append.hex
+
+# read utility functions, including movefiles
 source(file.path(codeloc, 'utility/wazefunctions.R'))
 
-for(mo in do.months){
-  prep.hex(file.path(inputdir, paste0("WazeTimeEdtHexWx_", mo, "_", HEXSIZE, "_mi.RData")), month = mo)
+# read random forest function
+source(file.path(codeloc, "analysis/RandomForest_WazeGrid_Fx.R"))
+
+# check if already completed transfer of necessary supplemental data on this instance
+if(length(dir(localdir)[grep("aadt_by_grid", dir(file.path(localdir, 'AADT')))]) == 0){
+  
+  source(file.path(codeloc, "utility/Workstation_setup.R"))
+  # move any files which are in an unnecessary "shapefiles" folder up to the top level of the localdir
+  system("mv -v ~/workingdata/shapefiles/* ~/workingdata/")
 }
 
-# Add FARS, AADT, HPMS, jobs
-na.action = "fill0"
-for(w in c("w.04", "w.05", "w.06", "w.07","w.08", "w.09")){
-  append.hex(hexname = w, data.to.add = "FARS_MD_2012_2016_sum_annual", na.action = na.action)
-  append.hex(hexname = w, data.to.add = "hexagons_1mi_routes_AADT_total_sum", na.action = na.action)
-  append.hex(hexname = w, data.to.add = "hexagons_1mi_routes_sum", na.action = na.action)
-  append.hex(hexname = w, data.to.add = "hexagons_1mi_bg_lodes_sum", na.action = na.action)
-  append.hex(hexname = w, data.to.add = "hexagons_1mi_bg_rac_sum", na.action = na.action)
-}
+# View the files available in S3 for this state: system(paste0('aws s3 ls ', teambucket, '/', state, '/'))
 
-w.04_09 <- rbind(w.04, w.05, w.06, w.07, w.08, w.09)
+# Loop over months to prepare for comparison
 
-# !!! Filter for days with good EDT data, meaning day > 259 in our initial sample.
-w.04_09 = w.04_09 %>% filter(as.numeric(day) < 259) 
+# <><><><><>
+states = c('CT', 'MD', 'UT','VA')  #'UT' # Sets the state. UT, VA, MD are all options.
+# <><><><><>
 
-avail.cores = parallel::detectCores()
+filestozip = vector()
 
-# Omit as predictors in this vector:
-alwaysomit = c(grep("GRID_ID", names(w.04), value = T), "day", "hextime", "year", "weekday", 
-               "uniqWazeEvents", "nWazeRowsInMatch", 
-               "nMatchWaze_buffer", "nNoMatchWaze_buffer",
-               grep("EDT", names(w.04), value = T))
+for(state in states){
+  # Load prepared input data
+  load(file.path(localdir, paste0(state, '_', do.months[1], '_to_', do.months[length(do.months)], '.RData')))
+  #assign(paste(state, "w.allmonths", sep="_"), w.allmonths)
+  #rm(w.allmonths)
 
-alert_types = c("nWazeAccident", "nWazeJam", "nWazeRoadClosed", "nWazeWeatherOrHazard")
-
-alert_subtypes = c("nHazardOnRoad", "nHazardOnShoulder" ,"nHazardWeather", "nWazeAccidentMajor", "nWazeAccidentMinor", "nWazeHazardCarStoppedRoad", "nWazeHazardCarStoppedShoulder", "nWazeHazardConstruction", "nWazeHazardObjectOnRoad", "nWazeHazardPotholeOnRoad", "nWazeHazardRoadKillOnRoad", "nWazeJamModerate", "nWazeJamHeavy" ,"nWazeJamStandStill",  "nWazeWeatherFlood", "nWazeWeatherFog", "nWazeHazardIceRoad")
-
-response.var = "MatchEDT_buffer_Acc"
-
-# 1. Waze data, April - September 2017 Maryland ----
-# Based on w.04_09 data frame from RandomForest_WazeGrid_Full.R
-# Output as Waze_04-09_GridCounts.csv
-# add counts of EDT damage and EDT injury (no, all 0 actually)
-
-# needed: 
-
-dim(w.04_09)
-
-# output for visualization
-w.out.for.viz <- w.04_09[c("GRID_ID", "day","hour","DayOfWeek",
-                           "nMatchEDT_buffer_Acc",
-                           "MatchEDT_buffer_Acc",
-                           "nWazeAccident","nWazeAccidentMajor","nWazeAccidentMinor",
-                           "uniqEDTreports",
-                           "uniqWazeEvents",
-                           "nEDTMaxDamDisabling")]
-
-dim(w.out.for.viz)
-
-write.csv(w.out.for.viz, file = "Waze_04-09_GridCounts.csv", row.names = F)
-
-# Also output additional variables
-
-# outputs supplemental variables by GRID_ID for Tableau work
-outputvars = c("GRID_ID",
-               c("CRASH_SUM", "FATALS_SUM"), # FARS variables, 
-               grep("F_SYSTEM", names(w.04), value = T), # road class
-               c("MEAN_AADT", "SUM_AADT", "SUM_miles"), # AADT
-               grep("WAC", names(w.04), value = T), # Jobs workplace
-               grep("RAC", names(w.04), value = T) # Jobs residential
-)
-
-w.supp = w.04[!duplicated(w.04$GRID_ID), outputvars]
-write.csv(w.supp, file = "Waze_GridCounts_Supplemental.csv", row.names = F)
-
-dim(w.supp)
-for(i in names(w.04[,outputvars])) cat(i, "\n")
-
-# 2. EDT data, April - mid September 2017 Maryland ----
-# Grid ID day hour dayofweek nEDT, fatal etc
-# 2M 8 col
-
-# 3. Model 30 outputs, April - September 2017 Maryland ----
-# All_Model_30.csv
-
-modelno = "30"
-omits = c(alwaysomit, alert_subtypes)
-
-s3load(object = file.path(paste(outputdir, sep="_"), paste("Model", modelno, "RandomForest_Output.RData", sep= "_")), bucket = waze.bucket)
-
-fitvars <- names(w.04_09)[is.na(match(names(w.04_09), omits))]
-
-head(out.df)
-
-# Re-fit with new cutoff of 0.28
-rf.pred <- predict(rf.out, w.04_09[testrows, fitvars], type = "response", cutoff = c(0.775, 0.225))
-levels(rf.pred) = c("NoCrash","Crash")
-rf.prob <- predict(rf.out, test.dat.use[fitvars], type = "prob", cutoff =  c(0.775, 0.225))
-reference.vec <- test.dat.use[,response.var]
-
-class(reference.vec) <- "data.frame"
-reference.vec <- as.factor(reference.vec[,1])
-levels(reference.vec) = c("NoCrash", "Crash")
-out.df <- data.frame(test.dat.use[, c("GRID_ID", "day", "hour", response.var)], rf.pred, rf.prob)
-out.df$day <- as.numeric(out.df$day)
-names(out.df)[4:7] <- c("Obs", "Pred", "Prob.Noncrash", "Prob.Crash")
-out.df = data.frame(out.df,
-                    TN = out.df$Obs == 0 &  out.df$Pred == "NoCrash",
-                    FP = out.df$Obs == 0 &  out.df$Pred == "Crash",
-                    FN = out.df$Obs == 1 &  out.df$Pred == "NoCrash",
-                    TP = out.df$Obs == 1 &  out.df$Pred == "Crash")
-
-
-
-
-w.test <- w.04_09[testrows,]
-
-identical(w.test$GRID_ID, out.df$GRID_ID)
-
-w.group <- data.frame(TN = out.df$TN, TP = out.df$TP, FP = out.df$FP, FN = out.df$FN)
-
-w.group$TN[w.group$TN==TRUE] = "TN"
-w.group$TP[w.group$TP==TRUE] = "TP"
-w.group$FP[w.group$FP==TRUE] = "FP"
-w.group$FN[w.group$FN==TRUE] = "FN"
-
-w.group$group <- apply(w.group, 1, function(x) x[x!=FALSE][1])
-
-grp  <- as.factor(w.group$group)
-
-#### Performance by time of day plot
-out.df$day <- as.character(out.df$day)
-
-dd <- data.frame(out.df, Pred.grp = grp, w.test[c("nMatchEDT_buffer_Acc", fitvars)]) 
-
-dim(dd)
-
-d2 <- dd %>% 
-  group_by(hour) %>%
-  summarize(N = n(),
-            TotalWazeAcc = sum(nWazeAccident),
-            TotalObservedEDT = sum(nMatchEDT_buffer_Acc),
-            TotalEstimated = sum(Pred == "Crash"),
-            Obs_Est_diff = TotalObservedEDT - TotalEstimated,
-            Pct_Obs_Est = 100 *TotalEstimated / TotalObservedEDT)
-
-ggplot(d2, aes(x = hour, y = Pct_Obs_Est)) + geom_line() + ylim(c(50, 120)) +
-  ylab("Percent of Observed EDT crashes Estimated")
-
-paste(rep(seq(0, 12, 2), 2), c("AM", "PM"))
-labs <- c("12 AM", "2 AM", "4 AM", "6 AM", "8 AM", "10 AM",
-          "12 PM", "2 PM", "4 PM", "6 PM", "8 PM", "10 PM")
-
-ggplot(d2, aes(x= hour, y= Pct_Obs_Est, fill = TotalWazeAcc)) +
-  geom_line(aes(y = 100, x = 0:23), lwd = 1.5, col = "darkgreen") +
-  geom_bar(stat="identity")+
-  coord_polar()+
-  ylim(c(0, 120)) + 
-  ylab("") +
-  xlab("Hour of Day") + 
-  scale_y_continuous(labels = "", breaks = 1) +
-  scale_x_continuous(labels = labs,
-                     breaks= seq(0, 23, 2)) +
+  # Load Model 30 outputs, April - September 2017 ----
+  # producing <state>_All_Model_30.csv
+  
+  modelno = "30"
+  response.var = "MatchEDT_buffer_Acc"
+  # Omit as predictors in this vector:
+  alwaysomit = c(grep("GRID_ID", names(w.allmonths), value = T), "day", "hextime", "year", "weekday", "vmt_time",
+                 "uniqWazeEvents", "nWazeRowsInMatch", 
+                 "nMatchWaze_buffer", "nNoMatchWaze_buffer",
+                 grep("EDT", names(w.allmonths), value = T))
+  
+  alert_types = c("nWazeAccident", "nWazeJam", "nWazeRoadClosed", "nWazeWeatherOrHazard")
+  
+  alert_subtypes = c("nHazardOnRoad", "nHazardOnShoulder" ,"nHazardWeather", "nWazeAccidentMajor", "nWazeAccidentMinor", "nWazeHazardCarStoppedRoad", "nWazeHazardCarStoppedShoulder", "nWazeHazardConstruction", "nWazeHazardObjectOnRoad", "nWazeHazardPotholeOnRoad", "nWazeHazardRoadKillOnRoad", "nWazeJamModerate", "nWazeJamHeavy" ,"nWazeJamStandStill",  "nWazeWeatherFlood", "nWazeWeatherFog", "nWazeHazardIceRoad")
+  
+  load(file.path(outputdir, paste(state, "Model", modelno, "RandomForest_Output.RData", sep= "_")))
+  
+  fitvars <- rownames(rf.out$importance)
+  
+  train.dat = w.allmonths
+  test.dat = w.allmonths # run on all hours of this time period, not 30% sample
+  
+  cutoff = c(0.775, 0.225)
+  cc <- complete.cases(test.dat[,fitvars])
+  test.dat <- test.dat[cc,]
+  
+  Fit_all.pred <- predict(rf.out, test.dat[fitvars], cutoff = cutoff)
+  Fit_all.prob <- predict(rf.out, test.dat[fitvars],  type = "prob", cutoff = cutoff)
+  
+  predtab <- table(test.dat[,response.var], Fit_all.pred)
+  
+  reference.vec <- test.dat[,response.var]
+  levels(reference.vec) = c("NoCrash", "Crash")
+  levels(Fit_all.pred) = c("NoCrash","Crash")
+  
+  reference.vec <-as.factor(as.character(reference.vec))
+  Fit_all.pred <-as.factor(as.character(Fit_all.pred))
+  
+  (predtab <- table(Fit_all.pred, reference.vec, 
+                    dnn = c("Predicted","Observed"))) 
+  bin.mod.diagnostics(predtab)
+  
+  model_auc <- pROC::auc(test.dat[,response.var], Fit_all.prob[,colnames(Fit_all.prob)=="1"])
+  
+  out.df <- data.frame(test.dat[, c("GRID_ID", "day", "hour", response.var)], Fit_all.pred, Fit_all.prob)
+  out.df$day <- as.numeric(out.df$day)
+  names(out.df)[4:7] <- c("Obs", "Pred", "Prob.Noncrash", "Prob.Crash")
+  out.df = data.frame(out.df,
+                      TN = out.df$Obs == 0 &  out.df$Pred == "NoCrash",
+                      FP = out.df$Obs == 0 &  out.df$Pred == "Crash",
+                      FN = out.df$Obs == 1 &  out.df$Pred == "NoCrash",
+                      TP = out.df$Obs == 1 &  out.df$Pred == "Crash")
+  
+  
+  head(out.df)
+  
+  w.group <- data.frame(TN = out.df$TN, TP = out.df$TP, FP = out.df$FP, FN = out.df$FN)
+  
+  w.group$TN[w.group$TN==TRUE] = "TN"
+  w.group$TP[w.group$TP==TRUE] = "TP"
+  w.group$FP[w.group$FP==TRUE] = "FP"
+  w.group$FN[w.group$FN==TRUE] = "FN"
+  
+  w.group$group <- apply(w.group, 1, function(x) x[x!=FALSE][1])
+  
+  grp  <- as.factor(w.group$group)
+  
+  out.df$Pred = ifelse(out.df$Pred=="Crash", 1, 0)
+  out.df$DayOfWeek = lubridate::wday(strptime(paste0("2017-", formatC(out.df$day, width = 3, flag = "0")), "%Y-%j"))
+  out.df$day <- as.character(out.df$day)
+  out.df$Hour <- strptime(paste0("2017-", formatC(out.df$day, width = 3, flag = "0"), " ", out.df$hour), "%Y-%j %H")
+  
+  dd <- data.frame(out.df, Pred.grp = grp, w.allmonths[c("nMatchEDT_buffer_Acc", fitvars, alert_subtypes)]) 
+  
+  #### Performance by time of day plot
+  
+  dim(dd)
+  
+  d2 <- dd %>% select(-Hour) %>% 
+    group_by(hour) %>%
+    summarize(N = n(),
+              TotalWazeAcc = sum(nWazeAccident),
+              TotalObservedEDT = sum(nMatchEDT_buffer_Acc),
+              TotalEstimated = sum(Pred == 1),
+              Obs_Est_diff = TotalObservedEDT - TotalEstimated,
+              Pct_Obs_Est = 100 *TotalEstimated / TotalObservedEDT)
+  
+  ggplot(d2, aes(x = hour, y = Pct_Obs_Est)) + geom_line() + ylim(c(80, 150)) +
+    ylab("Percent of Observed EDT crashes Estimated")
+  
+  paste(rep(seq(0, 12, 2), 2), c("AM", "PM"))
+  labs <- c("12 AM", "2 AM", "4 AM", "6 AM", "8 AM", "10 AM",
+            "12 PM", "2 PM", "4 PM", "6 PM", "8 PM", "10 PM")
+  
+  ggplot(d2, aes(x= hour, y= Pct_Obs_Est, fill = TotalWazeAcc)) +
+    geom_line(aes(y = 100, x = 0:23), lwd = 1.5, col = "darkgreen") +
+    geom_bar(stat="identity")+
+    coord_polar()+
+    ylim(c(0, 150)) + 
+    ylab("") +
+    xlab("Hour of Day") + 
+    scale_y_continuous(labels = "", breaks = 1) +
+    scale_x_continuous(labels = labs,
+                       breaks= seq(0, 23, 2)) +
   ggtitle("Model 30: Estimated EDT crashes / observed \n By hour of day")
+  ggsave(file = paste0(state, "_Obs_Est_rose.jpg"), device = 'jpeg', path = "~/workingdata/Figures")
+  
+  write.csv(d2, file.path(outputdir, paste0(state, "_Obs_Est_EDT_Model_30_by_hour.csv")), row.names = F)
+  write.csv(dd, file.path(outputdir, paste0(state, "_All_Model_30.csv")), row.names = F)
 
-write.csv(d2, "Obs_Est_EDT_Model_30_by_hour.csv", row.names = F)
-write.csv(dd, "All_Model_30.csv", row.names = F)
+  filestozip = c(filestozip,
+                 file.path(outputdir, paste0(state, "_Obs_Est_EDT_Model_30_by_hour.csv")),
+                 file.path(outputdir, paste0(state, "_All_Model_30.csv")))
+  
+} # end state loop ----  
+
+
+zipname = paste0('Multi-state_Model_', modelno, '_All_Output_', Sys.Date(), '.zip')
+
+system(paste('zip', file.path('~/workingdata', zipname),
+             paste(filestozip, collapse = " ")))
+
+system(paste(
+  'aws s3 cp',
+  file.path('~/workingdata', zipname),
+  file.path(teambucket, 'export_requests', zipname)
+))
+
+
 
 # Average by day
 
-# remake day of week, something went wrong -- should not have 0 and 7 both.
+# remake day of week
 datetime <- strptime(paste("2017", dd$day, sep = "-"), format = "%Y-%j")
 dd$DayOfWeek <- as.numeric(format(datetime, "%w")) # Weekday as decimal number (0–6, Sunday is 0).
 dd$Month <- as.character(format(datetime, "%B"))  # Full month name
--- analysis/RandomForest_WazeGrid_Model_18_compare.R
+++ analysis/RandomForest_WazeGrid_Model_18_compare.R
@@ -53,6 +53,10 @@ states = c('CT', 'MD')  #'UT' # Sets the state. UT, VA, MD are all options.
 # <><><><><>
 
 for(state in states){
+  
+  if(length(grep(paste0(state, '_', do.months[1], '_to_', do.months[length(do.months)], '.RData'), dir(localdir)))==0){
+    # Data prep ----
+    
   cat("Compiling input data for", state, "\n")
   # rename data files by month. For each month, prep time and response variables
   # See prep.hex() in wazefunctions.R for details.
@@ -60,16 +64,33 @@ for(state in states){
     prep.hex(paste0("WazeTimeEdtHexAll_", mo, "_", HEXSIZE, "mi_", state,".RData"), state = state, month = mo)
   }
   
-  monthfiles = paste("w", do.months, sep=".")
-  monthfiles = sub("-", "_", monthfiles)
-  
-  # Bind all months together, and remove monthly files to save memory
-  
-  w.allmonths <- vector()
-  for(i in monthfiles){
-    w.allmonths <- rbind(w.allmonths, get(i))
+    # Adding FARS, AADT, VMT, jobs
+    na.action = "fill0"
+    
+    
+    # Append supplmental data. This is now a time-intensive step, with hourly VMT; consider making this parallel
+    
+    for(w in monthfiles){ # w = "w.2017_04"
+      append.hex2(hexname = w, data.to.add = paste0("FARS_", state, "_2012_2016_sum_annual"), state = state, na.action = na.action)
+      append.hex2(hexname = w, data.to.add = paste0(state, "_max_aadt_by_grid_fc_urban_vmt_factored"), state = state, na.action = na.action)
+      append.hex2(hexname = w, data.to.add = paste0(state, "_hexagons_1mi_bg_wac_sum"), state = state, na.action = na.action)
+      append.hex2(hexname = w, data.to.add = paste0(state, "_hexagons_1mi_bg_rac_sum"), state = state, na.action = na.action)  
+    }
+    
+    
+    # Bind all months together, and remove monthly files to save memory
+    
+    w.allmonths <- vector()
+    for(i in monthfiles){
+      w.allmonths <- rbind(w.allmonths, get(i))
+    }
+    rm(list = monthfiles)
+    
+    save("w.allmonths", file = file.path(localdir, paste0(state, '_', do.months[1], '_to_', do.months[length(do.months)], '.RData')))
+    # end data prep
+  } else {
+    load(file.path(localdir, paste0(state, '_', do.months[1], '_to_', do.months[length(do.months)], '.RData')))
   }
-  rm(list = monthfiles)
   
   assign(paste(state, "w.allmonths", sep="_"), w.allmonths)
   rm(w.allmonths)
@@ -265,6 +286,152 @@ fn = paste0("Model_18_CT_mod_MD_data_Output.RData")
 
 save("keyoutputs", file = file.path(outputdir, fn))
 
+# Save outputs to S3
+
+system(paste("aws s3 cp",
+             file.path(outputdir, fn),
+             file.path(teambucket, "CT", fn)))
+
+# Model 30: MD model on CT data ----
+
+omits = c(alwaysomit, alert_subtypes,
+          #"wx",
+          #c("CRASH_SUM", "FATALS_SUM"), # FARS variables, added in 19
+          #grep("F_SYSTEM", names(w.allmonths), value = T), # road class
+          grep("SUM_MAX_AADT", names(MD_w.allmonths), value = T) # AADT
+          #grep("HOURLY_MAX_AADT", names(w.allmonths), value = T), # Hourly VMT
+          #grep("WAC", names(w.allmonths), value = T), # Jobs workplace
+          #grep("RAC", names(w.allmonths), value = T), # Jobs residential
+          #grep("MagVar", names(w.allmonths), value = T), # direction of travel
+          #grep("medLast", names(w.allmonths), value = T), # report rating, reliability, confidence
+          #grep("nWazeAcc_", names(w.allmonths), value = T), # neighboring accidents
+          #grep("nWazeJam_", names(w.allmonths), value = T) # neighboring jams
+)
+
+
+# Read model from local (pull down from S3 if not present). rf.out is the model, class randomForest
+
+load(file.path(outputdir, "MD_Model_30_RandomForest_Output.RData"))
+
+train.dat = MD_w.allmonths
+test.dat = CT_w.allmonths # run on all months of CT for now
+
+cutoff = c(0.775, 0.225)
+fitvars <- names(train.dat)[is.na(match(names(train.dat), omits))]
+cc <- complete.cases(test.dat[,fitvars])
+test.dat <- test.dat[cc,]
+
+MD_mod_CT_dat.pred <- predict(rf.out, test.dat[fitvars], cutoff = cutoff)
+MD_mod_CT_dat.prob <- predict(rf.out, test.dat[fitvars],  type = "prob", cutoff = cutoff)
+
+predtab <- table(test.dat[,response.var], MD_mod_CT_dat.pred)
+
+reference.vec <- test.dat[,response.var]
+levels(reference.vec) = c("NoCrash", "Crash")
+levels(MD_mod_CT_dat.pred) = c("NoCrash","Crash")
+
+reference.vec <-as.factor(as.character(reference.vec))
+MD_mod_CT_dat.pred <-as.factor(as.character(MD_mod_CT_dat.pred))
+
+(predtab <- table(MD_mod_CT_dat.pred, reference.vec, 
+                  dnn = c("Predicted","Observed"))) 
+bin.mod.diagnostics(predtab)
+
+model_auc <- pROC::auc(test.dat[,response.var], MD_mod_CT_dat.prob[,colnames(MD_mod_CT_dat.prob)=="1"])
+
+out.df <- data.frame(test.dat[, c("GRID_ID", "day", "hour", response.var)], MD_mod_CT_dat.pred, MD_mod_CT_dat.prob)
+out.df$day <- as.numeric(out.df$day)
+names(out.df)[4:7] <- c("Obs", "Pred", "Prob.Noncrash", "Prob.Crash")
+out.df = data.frame(out.df,
+                    TN = out.df$Obs == 0 &  out.df$Pred == "NoCrash",
+                    FP = out.df$Obs == 0 &  out.df$Pred == "Crash",
+                    FN = out.df$Obs == 1 &  out.df$Pred == "NoCrash",
+                    TP = out.df$Obs == 1 &  out.df$Pred == "Crash")
+
+savelist = c("rf.out", "rf.pred", "rf.prob", "out.df") 
+
+fn = paste("Model_30_MD_mod_CT_dat_RandomForest_Output.RData", sep= "_")
+
+save(list = savelist, file = file.path(outputdir, fn))
+
+# Copy to S3
+system(paste("aws s3 cp",
+             file.path(outputdir, fn),
+             file.path(teambucket, "MD", fn)))
+
+outlist =  list(predtab, diag = bin.mod.diagnostics(predtab), 
+                mse = mean(as.numeric(as.character(test.dat[,response.var])) - 
+                             as.numeric(MD_mod_CT_dat.prob[,"1"]))^2,
+                auc = as.numeric(model_auc) 
+) 
+
+keyoutputs[["Model_30_MD_mod_CT"]] = outlist
+
+fn = paste0("Model_30_MD_mod_CT_data_Output.RData")
+
+save("keyoutputs", file = file.path(outputdir, fn))
+
+# Run CT Model 30 on MD data ----
+
+load(file.path(outputdir, "CT_Model_30_RandomForest_Output.RData"))
+
+train.dat = CT_w.allmonths
+test.dat = MD_w.allmonths # run on all months of CT for now
+
+cutoff = c(0.775, 0.225)
+fitvars <- names(train.dat)[is.na(match(names(train.dat), omits))]
+cc <- complete.cases(test.dat[,fitvars])
+test.dat <- test.dat[cc,]
+
+CT_mod_MD_dat.pred <- predict(rf.out, test.dat[fitvars], cutoff = cutoff)
+CT_mod_MD_dat.prob <- predict(rf.out, test.dat[fitvars],  type = "prob", cutoff = cutoff)
+
+predtab <- table(test.dat[,response.var], CT_mod_MD_dat.pred)
+
+reference.vec <- test.dat[,response.var]
+levels(reference.vec) = c("NoCrash", "Crash")
+levels(CT_mod_MD_dat.pred) = c("NoCrash","Crash")
+
+reference.vec <-as.factor(as.character(reference.vec))
+CT_mod_MD_dat.pred <-as.factor(as.character(CT_mod_MD_dat.pred))
+
+(predtab <- table(CT_mod_MD_dat.pred, reference.vec, 
+                  dnn = c("Predicted","Observed"))) 
+bin.mod.diagnostics(predtab)
+
+model_auc <- pROC::auc(test.dat[,response.var], CT_mod_MD_dat.prob[,colnames(CT_mod_MD_dat.prob)=="1"])
+
+out.df <- data.frame(test.dat[, c("GRID_ID", "day", "hour", response.var)], CT_mod_MD_dat.pred, CT_mod_MD_dat.prob)
+out.df$day <- as.numeric(out.df$day)
+names(out.df)[4:7] <- c("Obs", "Pred", "Prob.Noncrash", "Prob.Crash")
+out.df = data.frame(out.df,
+                    TN = out.df$Obs == 0 &  out.df$Pred == "NoCrash",
+                    FP = out.df$Obs == 0 &  out.df$Pred == "Crash",
+                    FN = out.df$Obs == 1 &  out.df$Pred == "NoCrash",
+                    TP = out.df$Obs == 1 &  out.df$Pred == "Crash")
+
+savelist = c("rf.out", "rf.pred", "rf.prob", "out.df") 
+
+fn = paste("Model_30_CT_mod_MD_dat_RandomForest_Output.RData", sep= "_")
+
+save(list = savelist, file = file.path(outputdir, fn))
+
+# Copy to S3
+system(paste("aws s3 cp",
+             file.path(outputdir, fn),
+             file.path(teambucket, "CT", fn)))
+
+outlist =  list(predtab, diag = bin.mod.diagnostics(predtab), 
+                mse = mean(as.numeric(as.character(test.dat[,response.var])) - 
+                             as.numeric(CT_mod_MD_dat.prob[,"1"]))^2,
+                auc = as.numeric(model_auc) 
+) 
+
+keyoutputs[["Model_30_CT_mod_MD_dat"]] = outlist
+
+fn = paste0("Model_30_CT_mod_MD_data_Output.RData")
+
+save("keyoutputs", file = file.path(outputdir, fn))
 
 # Save outputs to S3
 
@@ -272,5 +439,45 @@ system(paste("aws s3 cp",
              file.path(outputdir, fn),
              file.path(teambucket, "CT", fn)))
 
+
 timediff <- Sys.time() - starttime
 cat(round(timediff, 2), attr(timediff, "units"), "to complete script")
+
+
+# Save all to export ----
+
+# Using system zip:
+# system(paste('zip ~/workingdata/zipfilename.zip ~/path/to/your/file'))
+
+zipname = paste0('VMT_RandomForest_Outputs_', Sys.Date(), '.zip')
+
+system(paste('zip', file.path('~/workingdata', zipname),
+             file.path(outputdir, 'MD_VMT_Output_to_30.RData'),
+             file.path(outputdir, 'CT_VMT_Output_to_30.RData'),
+             file.path(outputdir, 'Model_18_Output_to_CT.RData'),
+             file.path(outputdir, 'Model_18_CT_mod_MD_data_Output.RData'),
+             file.path(outputdir, 'Model_18_CT_mod_MD_dat_RandomForest_Output.RData'),
+             file.path(outputdir, 'Model_18_MD_mod_CT_data_Output.RData'),
+             file.path(outputdir, 'Model_18_MD_mod_CT_dat_RandomForest_Output.RData'),
+             file.path(outputdir, 'Model_30_CT_mod_MD_data_Output.RData'),
+             file.path(outputdir, 'Model_30_CT_mod_MD_dat_RandomForest_Output.RData'),
+             file.path(outputdir, 'Model_30_MD_mod_CT_data_Output.RData'),
+             file.path(outputdir, 'Model_30_MD_mod_CT_dat_RandomForest_Output.RData'),
+             file.path(outputdir, 'MD_Model_63_RandomForest_Output.RData'),
+             file.path(outputdir, 'MD_Model_62_RandomForest_Output.RData'),
+             file.path(outputdir, 'MD_Model_61_RandomForest_Output.RData'),
+             file.path(outputdir, 'MD_Model_30_RandomForest_Output.RData'),
+             file.path(outputdir, 'CT_Model_63_RandomForest_Output.RData'),
+             file.path(outputdir, 'CT_Model_62_RandomForest_Output.RData'),
+             file.path(outputdir, 'CT_Model_61_RandomForest_Output.RData'),
+             file.path(outputdir, 'CT_Model_30_RandomForest_Output.RData'),
+             file.path(outputdir, 'CT_Model_18_RandomForest_Output.RData'),
+             file.path(outputdir, 'MD_Model_18_RandomForest_Output.RData')
+))
+
+system(paste(
+  'aws s3 cp',
+  file.path('~/workingdata', zipname),
+  file.path(teambucket, 'export_requests', zipname)
+))
+
