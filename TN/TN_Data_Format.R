6fa6bab353f11d32ca5746328f4e9d5b77784c97
Dan Flynn <daniel.flynn@dot.gov>
2018-10-31 21:19:31 +0000

    Reading vwCollision

--- TN/TN_Data_Format.R
+++ TN/TN_Data_Format.R
@@ -21,10 +21,18 @@ setwd("~/workingdata/TN")
 # Data explore
 
 # Crash ----
-crash <-read.table("Crash/vwCollision.txt", sep = '|')
-
 
 # look at Crash/vwCollision.txt
+# First time need to read from txt, afterwards can read from RData, ~ 2 seconds vs 5 minutes. Every column is read as character.
+if(length(grep("TN_Crash.RData", dir('Crash')))==0){
+  crash <- read.csv("Crash/vwCollision.txt", sep = '|')
+  # format(object.size(crash), 'Mb')
+  save(crash, file = "Crash/TN_Crash.RData")
+} else {
+  load('Crash/TN_Crash.RData')
+}
+
+
 
 
 # Special Events ----
