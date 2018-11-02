7a9ac29ae0b47ae865d4b12de0beb091255677a7
Dan Flynn <daniel.flynn@dot.gov>
2018-11-02 20:01:09 +0000

    Chunks of yearmonths in unique alert_uuid aggregation

--- datacleaning/ReduceWaze_SDC.R
+++ datacleaning/ReduceWaze_SDC.R
@@ -129,110 +129,143 @@ cat("\n\n", round(td, 2), attr(td, "units"), "to complete all Waze_clip steps \n
 # First, loop over states again. Then parallel process the months. 
 # MD: > 11 Gb full spatial points data frame, saved after converting back to data.frame and removing unused columns, only 8.4 Gb. 
 
+# Originally did for entire time frame within a state. But If dataframe d is too large compared to free memory, cannot parallize. Have to re-make ym and also re-load for each portion in a loop. One idea was to check to see if d exceeds a certain % of available RAM. Saw in previous work that 5.8 Gb dataframe d (Maryland, 12 months) is too big to parallelize on 8 core 60 Gb instance.
+# Rule of thumb here: Calculate 2 * object size in mb / free memory. Test to see if this exceeds the ratio of 1/number of cores. 
+# Get available memory 
+# freem <- as.numeric(system("free -m | awk '/Mem:/ {print $4}'", intern = T))
+# 2*as.numeric(object.size(d)/1000000) / freem > 1/avail.cores
+  
+# Now simplifying. For each state, from the beginning just loop over some blocks months at a time. Cutting yearmonths into groups of four.
+  
 starttime <- Sys.time()
   
+ym.blocks = cut(1:length(yearmonths), breaks = 4, include.lowest = T)
+
 for(i in states){ # i = "UT"
   
-  # Pick out the right time zone for this state
-  use.tz <- tzs$tz[tzs$states == i]
-  
-  # Get the data from the S3 bucket if needed (to do), saving to workingdata
-  load(file.path("~/workingdata", paste0(i, "_Buffered_Clipped_events_to_", yearmonths[length(yearmonths)],".RData")))
-  
-  # Convert values to numeric
-  numconv <- c("report_rating", "confidence", "reliability", "pub_millis", "location_lon", "location_lat", "magvar")
-  
-  # Drop from spatial data frame back to data frame for easier aggregation. Will need to re-project for linking. Also makes object size smaller.
-  d <- d@data
-  d[numconv] <- apply(d[numconv], 2, function(x) as.numeric(x))
-  
-  # Year-month for looping
-  ym <- format(d$pub_utc_timestamp, "%Y-%m")
-  
-  # Removed unneeded variables to save ram
-  dropvars = c("report_description", "pub_utc_epoch_week", "num_thumbsup")
-  d <- d[,!names(d) %in% dropvars]
+  for(j in levels(ym.blocks)) { # j = levels(ym.blocks)[4]
+    
+    # Pick out the right time zone for this state
+    use.tz <- tzs$tz[tzs$states == i]
+    
+    # Get the data from the S3 bucket if needed (to do), saving to workingdata
+    load(file.path("~/workingdata", paste0(i, "_Buffered_Clipped_events_to_", yearmonths[length(yearmonths)],".RData")))
+    
+    # Convert values to numeric
+    numconv <- c("report_rating", "confidence", "reliability", "pub_millis", "location_lon", "location_lat", "magvar")
+    
+    # Drop from spatial data frame back to data frame for easier aggregation. Will need to re-project for linking. Also makes object size smaller.
+    d <- d@data
+    d[numconv] <- apply(d[numconv], 2, function(x) as.numeric(x))
+    
+    # Year-month for looping in parallel
+    ym <- format(d$pub_utc_timestamp, "%Y-%m")
 
-  # Manual 'loop' for MD. Have to re-make ym and also re-load for each half. Work on this to optimize when dataframe d is some % of available RAM. Now split between 1:8 and 9:17. 5.8 Gb is too big to parallelize on 8 core 60 Gb instance.
-  d <- d[ym %in% yearmonths[9:17],];  ym <- format(d$pub_utc_timestamp, "%Y-%m"); gc()
+    # Filter down to just this block of year-months
   
-  # Set up cluster. 
-  avail.cores <- parallel::detectCores()
-  if(avail.cores > length(yearmonths)) avail.cores = length(yearmonths) # use only cores necessary
-  cl <- makeCluster(avail.cores) 
-  registerDoParallel(cl)
+    d <- d[ym %in% yearmonths[ym.blocks == j],]
+    ym <- format(d$pub_utc_timestamp, "%Y-%m")
+    
+    # Removed unneeded variables to save ram
+    dropvars = c("report_description", "pub_utc_epoch_week", "num_thumbsup", "elt_run_id","alert_char_crc")
+    d <- d[,!names(d) %in% dropvars]
+    gc()
   
-# Start parallel loop over yearmonths within state ----
-foreach(mo = unique(ym), .packages = c("dplyr", "tidyr")) %dopar% {
-    # mo = "2018-04"
-    dx <- d[ym == mo,]
     
-    # dx = dx[1:5000,]
-    
-    
-    
-    ll <- dx %>%
-      group_by(alert_uuid) %>%
-      summarise(
-        median.reportRating = median(report_rating),
-        median.confidence = median(confidence),
-        median.reliability = median(reliability),
-        pubMillis = min(pub_millis),
-        lon = median(location_lon),
-        lat = median(location_lat),
-        last.time = min(pub_millis)+(sum(total_occurences)-1)*120000, #  120,000 milliseconds in 2 minutes. Sum of total occurrances across uuid_versions for this alert_uuid is the number of times it occurred in the curated data. Add to pub_millis to get last time. Subtract 1 because we are only interested in occurrance after the first one.
-        nrecord = n()
-      )
-    
-    # Additionally get confidence, reliability, report rating, and magvar for the most recent entry of this uuid. Can have fewer rows than part 1, ll.
-    ll2 <-  dx %>% 
-      group_by(alert_uuid) %>%
-      filter(pub_millis == max(pub_millis, na.rm = T)) %>%
-      summarize(
-        last.reportRating = median(report_rating),
-        last.confidence = median(confidence),
-        last.reliability = median(reliability),
-        magvar = median(magvar, na.rm = T)
-      )
-    
-    # part 3: get most frequent values for city, type, road_type, sub_type, and street. Fastest dplyr method is with slice(whic.max(n))
-
-    ll3 <- dx %>%
-      group_by(alert_uuid) %>%
-      count(city, alert_type, sub_type, street, road_type) %>%
-      slice(which.max(n)) 
-    
-    # join these two three tables
-    lx <- full_join(ll, ll2, by = 'alert_uuid')
-    lx <- full_join(lx, ll3, by = 'alert_uuid')
-    
-    # Format and output  
-    time <- as.POSIXct(lx$pubMillis/1000, origin = "1970-01-01", tz= use.tz) 
-    last.pull.time <- as.POSIXct(lx$last.time/1000, origin = "1970-01-01", tz= use.tz) 
-    
-    mb <- data.frame(lx, time, last.pull.time)
-    
-    filenamesave = paste(i, "_",
-                         mo,
-                         sep = "")
-     
-    save(list = "mb", 
-         file = file.path(output.loc, paste0(filenamesave, ".RData")))
-    
-    # Space after cp and before s3 for proper formatting
-    system(paste("aws s3 cp", 
-                 file.path(output.loc, paste0(filenamesave, ".RData")),
-                 file.path(teambucket, i, paste0(filenamesave, ".RData"))))
-    
-    timediff <- Sys.time() - starttime
-    
-    cat(round(timediff, 2), attr(timediff, "units"), "elapsed to save", i, "\n\n")
-    
-    cat(format(nrow(mb), big.mark = ","), "observations in aggregated data \n")
-  } # end month foreach loop
+    # Set up cluster. 
+    avail.cores <- parallel::detectCores()
+    if(avail.cores > length(yearmonths)) avail.cores = length(yearmonths) # use only cores necessary
+    cl <- makeCluster(avail.cores) 
+    registerDoParallel(cl)
   
-  rm(d, ym)
-  stopCluster(cl); gc()
+    # Start parallel loop over yearmonths within state ----
+    foreach(mo = unique(ym), .packages = c("dplyr", "tidyr")) %dopar% {
+        # mo = "2017-08"
+        dx <- d[ym == mo,]
+        
+        # dx = dx[1:5000,]
+      
+        # Calculate last.time differently for 2017 and 2018
+        if(substr(mo, 1, 4)==2017){
+            ll <- dx %>%
+              group_by(alert_uuid) %>%
+              summarise(
+                median.reportRating = median(report_rating),
+                median.confidence = median(confidence),
+                median.reliability = median(reliability),
+                pubMillis = min(pub_millis),
+                lon = median(location_lon),
+                lat = median(location_lat),
+                last.time = min(pub_millis)+(sum(total_occurences)-1)*300000, #  300,000 milliseconds in 5 minutes. Pull frequency for 2017 data (up to 2017-12-27)
+                # Sum of total occurrances across uuid_versions for this alert_uuid is the number of times it occurred in the curated data. Add to pub_millis to get last time. Subtract 1 because we are only interested in occurrance after the first one.
+                nrecord = n()
+            ) 
+            } else {
+              ll <- dx %>%
+                group_by(alert_uuid) %>%
+                summarise(
+                  median.reportRating = median(report_rating),
+                  median.confidence = median(confidence),
+                  median.reliability = median(reliability),
+                  pubMillis = min(pub_millis),
+                  lon = median(location_lon),
+                  lat = median(location_lat),
+                  last.time = min(pub_millis)+(sum(total_occurences)-1)*120000, #  120,000 milliseconds in 2 minutes. This is the pull frequency for 2018 data. 
+                  nrecord = n() 
+                )
+          } # end if/else for 2017 vs 2018 pull time 
+        
+        # Additionally get confidence, reliability, report rating, and magvar for the most recent entry of this uuid. Can have fewer rows than part 1, ll.
+        ll2 <-  dx %>% 
+          group_by(alert_uuid) %>%
+          filter(pub_millis == max(pub_millis, na.rm = T)) %>%
+          summarize(
+            last.reportRating = median(report_rating),
+            last.confidence = median(confidence),
+            last.reliability = median(reliability),
+            magvar = median(magvar, na.rm = T)
+          )
+        
+        # part 3: get most frequent values for city, type, road_type, sub_type, and street. Fastest dplyr method is with slice(whic.max(n))
+    
+        ll3 <- dx %>%
+          group_by(alert_uuid) %>%
+          count(city, alert_type, sub_type, street, road_type) %>%
+          slice(which.max(n)) 
+        
+        # join these two three tables
+        lx <- full_join(ll, ll2, by = 'alert_uuid')
+        lx <- full_join(lx, ll3, by = 'alert_uuid')
+        
+        # Format and output  
+        time <- as.POSIXct(lx$pubMillis/1000, origin = "1970-01-01", tz= use.tz) 
+        last.pull.time <- as.POSIXct(lx$last.time/1000, origin = "1970-01-01", tz= use.tz) 
+        
+        mb <- data.frame(lx, time, last.pull.time)
+        
+        filenamesave = paste(i, "_",
+                             mo,
+                             sep = "")
+         
+        save(list = "mb", 
+             file = file.path(output.loc, paste0(filenamesave, ".RData")))
+        
+        # Space after cp and before s3 for proper formatting
+        system(paste("aws s3 cp", 
+                     file.path(output.loc, paste0(filenamesave, ".RData")),
+                     file.path(teambucket, i, paste0(filenamesave, ".RData"))))
+        
+        timediff <- Sys.time() - starttime
+        
+        cat(round(timediff, 2), attr(timediff, "units"), "elapsed to save", i, "\n\n")
+        
+        cat(format(nrow(mb), big.mark = ","), "observations in aggregated data \n")
+      } # end month foreach loop
+      
+    rm(d, ym)
+    stopCluster(cl); gc()
+    
+  } # end ym blocks
   
   timediff <- round(Sys.time()-starttime, 2)
   cat(i, "complete \n", 
