41bb0af77f2835a66631553241160aa9e3ee2e1d
Ci (Jessie) Yang <31483478+cyang-volpe@users.noreply.github.com>
2018-11-28 16:27:47 -0500

    Crash analysis initial commit

---
+++ TN/analysis/Crash_Analysis_TN.R
@@ -0,0 +1,150 @@
+# Tennesee crash data initial formatting and exploration
+# Jessie prepared this one for Jonathan to start with some simple data analysis.
+
+# Setup ----
+
+# Check for package installations
+getwd() # this is the default working directory, usually will be the "C:/Users/[your user name]/Documents"
+codeloc <- "~/GitHub/SDI_Waze" # you'll see the R code location once you cloned the GitHub repository
+dataloc <- "//vntscex.local/DFS/Projects/PROJ-OS62A1/SDI Waze Phase 2/Data/TN/Crash"
+library(tidyverse) 
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+
+# Customized Functions
+sumstats = function(x) { 
+  null.k <- function(x) sum(is.na(x))
+  unique.k <- function(x) {if (sum(is.na(x)) > 0) length(unique(x)) - 1
+    else length(unique(x))}
+  range.k <- function(x) max(x, na.rm=TRUE) - min(x, na.rm=TRUE)
+  mean.k=function(x) {if (is.numeric(x)) round(mean(x, na.rm=TRUE), digits=2)
+    else "N*N"} 
+  sd.k <- function(x) {if (is.numeric(x)) round(sd(x, na.rm=TRUE), digits=2)
+    else "N*N"} 
+  min.k <- function(x) {if (is.numeric(x)) round(min(x, na.rm=TRUE), digits=2)
+    else "N*N"} 
+  q05 <- function(x) quantile(x, probs=.05, na.rm=TRUE)
+  q10 <- function(x) quantile(x, probs=.1, na.rm=TRUE)
+  q25 <- function(x) quantile(x, probs=.25, na.rm=TRUE)
+  q50 <- function(x) quantile(x, probs=.5, na.rm=TRUE)
+  q75 <- function(x) quantile(x, probs=.75, na.rm=TRUE)
+  q90 <- function(x) quantile(x, probs=.9, na.rm=TRUE)
+  q95 <- function(x) quantile(x, probs=.95, na.rm=TRUE)
+  max.k <- function(x) {if (is.numeric(x)) round(max(x, na.rm=TRUE), digits=2)
+    else "N*N"} 
+  
+  sumtable <- cbind(as.matrix(colSums(!is.na(x))), sapply(x, null.k), sapply(x, unique.k), sapply(x, range.k), sapply(x, mean.k), sapply(x, sd.k),
+                    sapply(x, min.k), sapply(x, q05), sapply(x, q10), sapply(x, q25), sapply(x, q50),
+                    sapply(x, q75), sapply(x, q90), sapply(x, q95), sapply(x, max.k)) 
+  
+  sumtable <- as.data.frame(sumtable); names(sumtable) <- c('count', 'null', 'unique',
+                                                            'range', 'mean', 'std', 'min', '5%', '10%', '25%', '50%', '75%', '90%',
+                                                            '95%', 'max') 
+  return(sumtable)
+} 
+
+# Set up working directory
+setwd("~/workingdata/TN") # you can create your own working folder in any of your local directory
+
+# Data explore
+
+# Crash ----
+# look at Crash/vwCollision.txt
+# First time need to read from txt, afterwards can read from RData, ~ 2 seconds vs 5 minutes. Every column is read as character. Create the crash data as an R data file or load the already exist R file. 
+if(length(grep("TN_Crash.RData", dir('Crash')))==0){
+  crash <- read.csv(paste0(dataloc,"/vwCollision.txt"), sep = '|')
+  # format(object.size(crash), 'Mb')
+  save(crash, file = "Crash/TN_Crash.RData")
+} else {
+  load('Crash/TN_Crash.RData')
+}
+
+# examine the crash data
+names(crash)
+dim(crash) # 829,301 rows * 80 columns
+
+# Crash ID, it is unique? Yes.
+length(unique(crash$MstrRecNbrTxt)) # 829,301, looks like this is a unique crash ID
+
+table(crash$NbrUnitsNmb) # this column could include more than the number of vehicles
+# 0      1      2      3      4      5      6      7      8      9     10     11     12     13     14     15     17     18     22     26     98 
+# 3 219063 565583  37956   5359    969    243     73     18      9      8      4      1      2      1      1      1      1      1      1      1
+
+table(crash$CrashTypeCde) # need information from TN State Petrol for more information
+# 0      1      2      3      4      5      6     80     98 
+# 21   3627  21418  54944 114219 574395  60574     96      1 
+
+# select the necessary columns for analysis (optional: save it as subset in the RData again?)
+var <- c("MstrRecNbrTxt" # Unique crash ID
+         , "CollisionDte" # Date of Crash
+         , "CollisionTimeTxt" # Time of Crash
+         , "NbrUnitsNmb" # Number of vehicles involved
+         , "NbrFatalitiesNmb" # Fatals
+         , "NbrInjuredNmb" # Injuries
+         , "NbrNonInjuredNmb" # Non-Inguries
+         , "NbrMotoristsNmb" # Number of drivers
+         , "AlcoholInd" # whether alchohol is involved
+         , "BlockNbrTxt" # Does not look like Census block
+         , "CityCde" # City ID
+         , "CountyStateCde" # County ID
+         , "CrashTypeCde"  # What crash type do they have?
+         , "IntersectionInd" # Whether it is an intersection             
+         , "IntersectLocalIDTxt" # Intersection Location ID
+         , "IntersectRoadNameTxt" # Intersection road name
+         , "IntersectRoadNameSuffixTxt" # Name suffix
+         , "IntersectRoadNbrTxt" # Intersection road/route number
+         , "LatDecimalNmb" # Lat
+         , "LongDecimalNmb" # Lon
+         , "LightConditionCde" # light condition
+         ) 
+crash <- crash[,var]
+
+# Data completeness
+# crash[!complete.cases(crash),] # Error: C stack usage  8200812 is too close to the limit, the data is stil too large
+# format(object.size(crash), "Mb") = 425 Mb
+
+# Further reduce the data
+var1 <- c("MstrRecNbrTxt", "CollisionDte", "LatDecimalNmb", "LongDecimalNmb") # only take ID, time, and location
+crash1 <- crash[, var1]
+crash1[!complete.cases(crash1),] # display rows with missing data, looks like most of them are just missing the geo-coordinates.
+
+colSums(is.na(crash1)) # Number of missing data.
+colSums(is.na(crash1))*100/nrow(crash1) # percent of missing data, ~29% missing Lat/Lon
+
+round(colSums(is.na(crash))*100/nrow(crash), 2) # Large amounts of data are missing for some columns, such as BlockNbrTxt, IntersectionInd, IntersectLocalIDTxt, IntersectRoadNameTxt. If we plan to use these columns, need to understand why the missing data are not captured in is.na(). This might be due to blanks. 
+
+
+# Looking at missing location info by year
+crash$date = as.POSIXct(strptime(crash$CollisionDte, "%Y-%m-%d %H:%M:%S", tz = "US/Central"))
+crash$year = as.numeric(format(crash$date, "%Y"))
+# Fix data types
+crash$MstrRecNbrTxt <- as.character(crash$MstrRecNbrTxt)
+
+na.lat <- crash %>%
+  filter(!is.na(year)) %>%
+  group_by(fatal = NbrFatalitiesNmb > 0 , year) %>%
+  summarize(naLat = sum(is.na(LatDecimalNmb) | LatDecimalNmb <= -99),
+            N = n(),
+            pct.na = 100*naLat/N)
+
+ggplot(na.lat) +
+  geom_point(aes(x = as.factor(year), y = pct.na, color = fatal)) +
+  ylab("Percent missing Lat/Long") + xlab("Year") + 
+  ggtitle("Summary of missing location by year and crash fatality for Tennessee")
+ggsave("Output/Missing_lat_TN.jpeg")
+
+
+var1 <- c("MstrRecNbrTxt", "date", "year", "LatDecimalNmb", "LongDecimalNmb") # only take ID, time, and location
+crash1 <- crash[, var1]
+
+sumstats(crash1[2:ncol(crash1)])
+
+
+tn_crash <- crash1 %>% filter(LatDecimalNmb > 25 & LatDecimalNmb < 45 &
+         LongDecimalNmb < -75 & LongDecimalNmb > -99)
+
+sumstats(tn_crash[2:ncol(tn_crash)])
+
+save("tn_crash", file = "Crash/TN_Crash_Simple_2008-2018.RData")
+
