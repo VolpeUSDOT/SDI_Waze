0447e6cf3b79ac98f11c302bae4e051093d32b0d
dflynn-volpe <daniel.flynn@dot.gov>
2019-02-08 13:50:01 -0500

    Update to RF local evaluation script
    Needs work still

--- TN/analysis/RF_Model_Eval_Re-fit_local.R
+++ TN/analysis/RF_Model_Eval_Re-fit_local.R
@@ -14,6 +14,7 @@ grids = c("TN_01dd_fishnet",
 g = grids[1] # Manually select 1 or 2, or can build a loop.
 modelno = "06" # 01 to 06, manually select or loop
 version = "2019-02-07"
+state = "TN"
 # <><><><><><>
 
 # Load full input data
@@ -23,33 +24,6 @@ w.all <- read.csv(file.path(volpedrive, paste0("Random_Forest_Output_", version)
 load(file.path(volpedrive, paste0("Random_Forest_Output_", version), paste0("TN_Model_", modelno, "_", g, "_RandomForest_Output.RData")))
 
 # Visualizations ----
-
-# pdf(file.path(volpedrive, "Figs", paste0("Visualzing_classification_April-Sept_2017_Model_",  modelno, ".pdf")))
-
-out.df <- out.df %>%
-  mutate(Obs.bin = Obs > 0)
-
-ggplot(out.df, aes(Prob.Crash, fill = Obs.bin)) + 
-  geom_histogram(alpha = 0.5, position = "identity", binwidth = 0.1) +
-  ggtitle("Probability of Waze event being categorized as crash")
-
-ggplot(out.df, aes(Prob.Crash, fill = Obs.bin)) + 
-  geom_histogram(alpha = 0.5, position = "identity", binwidth = 0.001) + 
-  ylim(0, 500) +
-  ggtitle("Probability of Waze event being categorized as crash /n (Truncated at count = 500)")
-
-ggplot(out.df, aes(Prob.Crash, fill = Pred)) + 
-  geom_histogram(alpha = 0.5, position = "identity", binwidth = 0.005) + 
-  ylim(0, 500) +
-  ggtitle("Classification Waze event as crash \n (Truncated at count = 500)")
-
-ggplot(out.df, aes(Prob.Crash, fill = Pred)) + 
-  geom_histogram(alpha = 0.5, position = "identity", binwidth = 0.01) + 
-  ylim(0, 500) +
-  facet_wrap(~Obs.bin) +
-  ggtitle("Classification Waze event as crash by observed values \n (Truncated at count = 500)")
-
-# dev.off()
 
 ### Make aggregation of Waze accidents / minor / major by grid ID
 w.grid <- w.all %>%
@@ -65,7 +39,147 @@ ggplot(w.grid) +
   facet_wrap(~Urban) +
   geom_abline(slope = 1, intercept = 0)
 
+# variable importance ----
+pdf(file.path(localdir, "Figures", paste0(state, "_Variable_Importance_Model_", modelno, ".pdf")), width = 8, height = 8)
+
+varImpPlot(rf.out, 
+           main = paste(state, "Model",modelno,"Variable Importance"),
+           bg = scales::alpha("midnightblue", 0.5),
+           n.var = 15)
+
+dev.off()
+# Classification ----
+pdf(file.path(localdir, "Figures", paste0(state, "_Visualzing_classification_Model_", modelno, ".pdf")), width = 10, height = 10)
+
+out.df$CorrectPred = out.df$TN | out.df$TP
+levels(out.df$Obs) = c("Obs = NoCrash", "Obs = Crash")
+
+gp1 <- ggplot(out.df, aes(Prob.Crash, fill = Obs)) + 
+  geom_histogram(alpha = 0.5, position = "identity", binwidth = 0.01) + 
+  scale_y_continuous(limits=c(0, 5000), oob = scales::rescale_none) + 
+  ggtitle("Probability of Waze event being categorized as EDT crash \n (Truncated at count = 5000)") + 
+  geom_vline(xintercept = 0.225, linetype = 'dotted') +
+  annotate("text", x = 0.325, y = 5000, label = "Cutoff = 0.225") +
+  xlab("Estimated Crash Probability") +
+  #theme_bw() + 
+  scale_fill_brewer(palette="Set1")
+
+print(gp1)
+
+gp2 <- ggplot(out.df, aes(Prob.Crash, fill = CorrectPred)) + 
+  geom_histogram(alpha = 0.5, position = "identity", binwidth = 0.01) + 
+  scale_y_continuous(limits=c(0, 5000), oob = scales::rescale_none) + 
+  geom_vline(xintercept = 0.225, linetype = 'dotted') +
+  facet_wrap(~Obs.bin) +
+  xlab("Estimated Crash Probability") +
+  scale_fill_brewer(palette="Set1") +
+  ggtitle("Frequency of classification as crash by observed values \n (Truncated at count = 500; Max = 600,000)")
+
+print(gp2)
+# Plotting historgram of difference from observed and estimated, by grid cell aggregated over time ----
+levels(out.df$Pred) = c(1, 0) # from Crash, NoCrash
+levels(out.df$Obs.bin) = c(0, 1) # from Obs  =NoCrash, Obs = Crash
+
+pct.diff.grid <- out.df %>%
+  mutate(nObs = as.numeric(as.character(Obs.bin)),
+         nPred = as.numeric(as.character(Pred))) %>%
+  group_by(GRID_ID) %>%
+  summarize(sumObs = sum(nObs),
+            sumPred = sum(nPred),
+            Pct.diff = 100*(sumPred - sumObs) / sumObs)
+
+pct.diff.grid$Pct.diff[is.na(pct.diff.grid$Pct.diff) | pct.diff.grid$Pct.diff == Inf] = 0
+
+#hist(pct.diff.grid$Pct.diff)
+
+# Aggregate for report table
+pct.diff.table = as.data.frame(table(pct.diff.cut <- cut(pct.diff.grid$Pct.diff, breaks = c(-2000, -100, -50, -1, 0, 50, 100, 2000))))
+# https://drsimonj.svbtle.com/pretty-histograms-with-ggplot2. Trick is fill = cut()
+
+# levels(cut(pct.diff.grid$Pct.diff, 25)) # create manual colors to match tablesu 
+
+gp3 <- ggplot(pct.diff.grid, aes(Pct.diff, fill = cut(Pct.diff, 
+                                                      breaks = c(-1000, -100, -50, -25, 
+                                                                 0, 
+                                                                 25, 50, 100, 1000) ))) + 
+  geom_histogram(bins = 10, show.legend = F, binwidth = 10) +
+  theme_dark() +
+  ggtitle("Summary of percent difference from observed and estimated EDT-level crashes") +
+  xlab("Percent difference") + ylab("Frequency by GRID_ID") +
+  scale_fill_brewer(palette = "RdBu", direction = -1)
+#
+#  scale_color_gradient(low = 'red', high = 'midnightblue')
+
+print(gp3)
+# Choosing cutoffs ----
+# Low value is most greedy for non-crashes, high value is more greedy for crashes
+response.var = 'TN_crash'
+
+fitvars <- rownames(rf.out$importance)
+test.dat.use = w.all[testrows,]
+reference.vec <- test.dat.use[,response.var]
+levels(reference.vec) = c("NoCrash", "Crash")
+
+co = seq(0.1, 0.9, by = 0.1)
+pt.vec <- vector()
+for(i in co){
+  predx <- predict(rf.out, w.all[testrows, fitvars], type = "response", cutoff = c(1-i, i))
+  levels(predx) = c("NoCrash","Crash")
+  
+  predx <-as.factor(as.character(predx)) # Crash is first
+  reference.vec <-as.factor(as.character(reference.vec)) # Same
+  
+  predtab <- table(predx, reference.vec, 
+                   dnn = c("Predicted","Observed")) 
+  if(sum(dim(predtab))==4) {bin.diag = bin.mod.diagnostics(predtab)} else {bin.diag = NA}
+  
+  pt.vec <- cbind(pt.vec, bin.diag)
+  cat(i, ". ")
+}
+colnames(pt.vec) = co
+assign(paste0(state, '_pt.vec'), pt.vec)
+
+# High precision: minimize false positives. Achieved with the strictest requirement for classifying as a crash
+# High recall (sensitivity): minimize false negatives. Acheived with the least strict requrirement for classifiying as a crash
+
+# Try to make a nicer version in ggplot
+library(tidyr)
+
+prec.recall <- as.data.frame(pt.vec) %>%
+  gather()
+
+prec.recall$Metric = rep(c(1, 3, 2, 4), ncol(pt.vec))
+prec.recall$Metric <- factor(prec.recall$Metric, labels = c("Accuracy", "Recall","Precision", "False Positive Rate"))
+names(prec.recall)[1:2] = c("Cutoff", "Value")
+
+gp4 <- ggplot(prec.recall, aes(x = Cutoff, y = Value, group = Metric)) + 
+  geom_line(aes(color = Metric), size = 2) +
+  ggtitle(paste0(state, "Crash classification tradeoffs, April-September 2017 \n Model ", modelno)) + 
+  theme_bw() +
+  annotate("text",
+           x = 0.5,
+           y = c(0.95, 0.05, 0.46,0.81),
+           hjust = 0,
+           label = c("Accuracy", "False Positive Rate", "Precision", "Recall"))
+print(gp4)      
+
+dev.off()
+
+
+
 # TODO 2019-02-07 add model refitting steps. Below is old from first iteration <><><><><><>
+
+# RF inputs
+response.var = "MatchEDT_buffer_Acc"
+alwaysomit = c(grep("GRID_ID", names(w.allmonths), value = T), "day", "hextime", "year", "weekday", "vmt_time",
+               "uniqWazeEvents", "nWazeRowsInMatch", 
+               "nMatchWaze_buffer", "nNoMatchWaze_buffer",
+               grep("EDT", names(w.allmonths), value = T))
+
+alert_types = c("nWazeAccident", "nWazeJam", "nWazeRoadClosed", "nWazeWeatherOrHazard")
+
+alert_subtypes = c("nHazardOnRoad", "nHazardOnShoulder" ,"nHazardWeather", "nWazeAccidentMajor", "nWazeAccidentMinor", "nWazeHazardCarStoppedRoad", "nWazeHazardCarStoppedShoulder", "nWazeHazardConstruction", "nWazeHazardObjectOnRoad", "nWazeHazardPotholeOnRoad", "nWazeHazardRoadKillOnRoad", "nWazeJamModerate", "nWazeJamHeavy" ,"nWazeJamStandStill",  "nWazeWeatherFlood", "nWazeWeatherFog", "nWazeHazardIceRoad")
+
 
 omits = c(alwaysomit, alert_subtypes)
 
