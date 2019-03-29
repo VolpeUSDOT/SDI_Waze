e9272ff322c581d9e42b4fa0716ac142f46a5289
dflynn-volpe <daniel.flynn@dot.gov>
2019-03-29 14:43:53 -0400

    Completed make space time segment script

--- WA/datacleaning/MakeSpaceTimeSegments_Bell.R
+++ WA/datacleaning/MakeSpaceTimeSegments_Bell.R
@@ -1,4 +1,4 @@
-# Precursor to Grid_aggregation, follows Hex_UA_overlay_SDC
+# Precursor to Segment_Aggregation_Bell.R
 
 
 # <><><><><><><><><><><><><><><><><><><><>
@@ -10,6 +10,7 @@ library(rgdal) # for readOGR(),writeOGR () needed for reading/writing in ArcM sh
 library(rgeos) # gintersection
 library(ggmap)
 library(tidyverse)
+library(doParallel)
 
 codeloc <- ifelse(grep('Flynn', normalizePath('~/')),
                   "~/git/SDI_Waze", "~/GitHub/SDI_Waze")
@@ -24,7 +25,7 @@ data.loc <- file.path(wazeshareddir, "Data/Bellevue")
 # GIS layer projection
 proj <- showP4(showWKT("+init=epsg:6597"))
 
-# Load the network data
+# Load the network data, for the definitive RDSEG_ID to join to
 roadnettb_snapped <- readOGR(dsn = file.path(data.loc, "Shapefiles"), layer = "RoadNetwork_Jurisdiction_withData") # 6647 * 14, new: 6647*38
 roadnettb_snapped <- spTransform(roadnettb_snapped, CRS(proj))
 
@@ -33,14 +34,17 @@ waze_snapped <- readOGR(dsn = file.path(data.loc, "Shapefiles"), layer = "Waze_S
 waze_snapped <- spTransform(waze_snapped, CRS(proj))
 
 # Load unsnapped Waze data as well. time variable was reduced to just date in the shapefile, so we will join with the original data to get precise time
-wazetb <- read_csv(file.path(data.loc, 'Export', 'WA_Bellevue_Prep_Export.csv'))
-# format timestamp
-wazetb$time <- as.POSIXct(wazetb$time, tz = "America/Los_Angeles")
+# TODO: Export again with last.pull.time, also add that to the join
+
+wazetb <- read_csv(file.path(data.loc, 'Export', 'WA_Bellevue_Prep_Export.csv'),
+                   locale = locale(tz = 'America/Los_Angeles'))
+
 # Getting the timestamp of the Waze events from the original Waze data export
 waze_snapped@data <- left_join(waze_snapped@data %>% select(-time),
-                               waze_unsnapped %>% select(SDC_uuid, time),
+                               wazetb %>% select(SDC_uuid, time),
                                by = 'SDC_uuid') 
-stopifnot(sum(is.na(waze_snapped@data$time)) == 0) # check if there is any NA.
+stopifnot(sum(is.na(waze_snapped$time)) == 0) # check if there are any NAs.
+stopifnot(any(class(waze_snapped$time) %in% 'POSIXct')) # time is correctly formatted.
 
 # Load Crash data ----
 crash_snapped <- readOGR(dsn = file.path(data.loc, "Shapefiles"), layer = "Crashes_Snapped50ft_MatchName") # 2085*29, new: 1369*51 
@@ -48,150 +52,111 @@ crash_snapped <- spTransform(crash_snapped, CRS(proj))
 
 # Load unsnapped crash data for confirmation on date
 # Raw Crash points - potential to add additional variables to the final model
-# crashtb <- read.csv(file = file.path(data.loc, "Crash","20181127_All_roads_Bellevue.csv")) # this is the old crash data
-crashtb <- read.csv(file = file.path(data.loc, "Crash","20190314_All_roads_Bellevue.csv")) # this is the old crash data
-names(crashtb) # Looks like this is a full crash database
-dim(crashtb) # 2825  254, a total of 4417 crashes.
+crashtb <- read.csv(file = file.path(data.loc, "Crash","20190314_All_roads_Bellevue.csv")) # 2018
+# names(crashtb) # Looks like this is a full crash database
+# dim(crashtb) # 2825  254, a total of 4417 crashes.
 
 # format timestamp
 crashtb$time <- as.POSIXct(paste(crashtb$DATE, crashtb$X24.HR.TIME), tz = 'America/Los_Angeles', format = "%m/%d/%Y %H:%M")
 # join the crash snapped data with additional column time
-crash_snapped@data <- left_join(crash_snapped@data %>% select(-time),
+crash_snapped@data <- left_join(crash_snapped@data,
                                 crashtb %>% select(REPORT.NUMBER, time),
                                 by = c("REPORT_NUM" = "REPORT.NUMBER"))
-stopifnot(sum(is.na(crash_snapped@data$time)) == 0) # check if there is any NA.
+stopifnot(sum(is.na(crash_snapped$time)) == 0) # check if there are any NAs.
+stopifnot(any(class(crash_snapped$time) %in% 'POSIXct')) # time is correctly formatted.
 
-# Load weather data -- Only need to assign to a Date
-load(file.path(data.loc, "Weather","Prepared_Bellevue_Wx_2018.RData")) # the file name wx.grd.day
-# format day
-wx.grd.day$day <- as.Date(wx.grd.day$day)
 
 # <><><><><><><><><><><><><><><><><><><><>
-# In Process ----
+# Process to monthly segment x hour files ----
 # Segment unique ID: RDSEG_ID
-SegIDall <- roadnettb_snapped$RDSEG_ID
+SegIDall <- roadnettb_snapped$RDSEG_ID # 6647 segments 
 
 # All year, month, and hour
 # Calendar year 2018
-# # Create date field for Waze from time 
-# waze_snapped$date <- format(waze_snapped$time, format='%Y-%m-%d')
-# crash_snapped$DATE <- as.Date(as.character(crash_snapped$DATE))
-
-# list of all months
+# list of all months to do. 
 todo.months = format(seq(from = as.Date("2018-01-01"), to = as.Date("2018-12-31"), by = "month"), "%Y-%m")
 
-# Format month, day
-year.month.w <- unique(format(waze_snapped$time, "%Y-%m"))
-year.month.c <- unique(format(crash_snapped$time, "%Y-%m"))
-year.month <- year.month.w
-year.month[is.na(year.month)] <- year.month.t[is.na(year.month)]
+# Format month, day. Use these for indexing within the loop. Keep track of .w and .c
+year.month.w <- format(waze_snapped$time, "%Y-%m")
+year.month.c <- format(crash_snapped$time, "%Y-%m")
 
 # Start parallel process
 cl <- makeCluster(parallel::detectCores()) 
 registerDoParallel(cl)
 
-foreach(j = todo.months, .packages = c("dplyr", "lubridate", "utils")) %dopar% {
-  j = todo.months[1]
+# Store messages in a log file
+writeLines(c(""), "Make_SpaceTime_log.txt")    
 
-    link.waze.bell.j <- link.waze.bell[year.month == j,]
+foreach(j = todo.months, .packages = c("dplyr", "lubridate", "utils")) %dopar% {
+  sink("Make_SpaceTime_log.txt", append=TRUE)
+  cat(paste(Sys.time()), j, "\n")                                                
+  # j = todo.months[1]
+  waze.j <- waze_snapped[year.month.w == j,]
+  crash.j <- crash_snapped[year.month.c == j,]
+
+  month.days.w  <- unique(as.numeric(format(waze.j$time, "%d"))) # Waze event date/time
+  month.days.c  <- unique(as.numeric(format(waze.j$time, "%d"))) # Bellevue crash date/time
+  month.days = sort(unique(c(month.days.w[!is.na(month.days.w)], month.days.c[!is.na(month.days.c)])))
+  
+  lastday = max(month.days[!is.na(month.days)])
+ 
+   # A list of timezone areas for reference: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  Month.hour <- seq(from = as.POSIXct(paste0(j,"-01 0:00"), tz = 'America/Los_Angeles'), 
+                    to = as.POSIXct(paste0(j,"-", lastday, " 24:00"), tz = 'America/Los_Angeles'),
+                    by = "hour")
+  
+  # Make a segment time all table
+  SegIDTime <- expand.grid(Month.hour, SegIDall)
+  names(SegIDTime) <- c("SegDayHour", "RDSEG_ID")
+  
+  StartTime <- Sys.time()
+  t.min = min(SegIDTime$SegDayHour) 
+  t.max = max(SegIDTime$SegDayHour) 
+  i = t.min
+  
+  Waze.seg.time.all <- vector()
+  Crash.seg.time.all <- vector()
+  
+  counter = 1
+  while(i+3600 <= t.max){
+    ti.SegIDTime = filter(SegIDTime, SegDayHour == i)
+    # Match Waze events by time time. Use last pull time if available
+    if('last.pull.time' %in% names(waze.j)){
+      ti.link.waze = waze.j@data %>% filter(time >= i & time <= i+3600 | last.pull.time >= i & last.pull.time <=i+3600) 
+    } else {
+      ti.link.waze = waze.j@data %>% filter(time >= i & time <= i+3600)  
+      }
     
-    month.days.w  <- unique(as.numeric(format(waze_snapped$time, "%d"))) # Waze event date/time
-    month.days.c  <- unique(as.numeric(format(crash_snapped$DATE, "%d"))) # Bellevue crash date/time
-    month.days = sort(unique(c(month.days.w[!is.na(month.days.w)], month.days.t[!is.na(month.days.t)])))
+    # Match crash events by time
+    ti.link.crash = crash.j@data %>% filter(time >= i & time <= i+3600) 
     
-    lastday = max(month.days[!is.na(month.days)])
+    ti.Waze.seg.w <- inner_join(ti.SegIDTime, ti.link.waze, by = "RDSEG_ID") 
+    ti.Waze.seg.c <- inner_join(ti.SegIDTime, ti.link.crash, by = "RDSEG_ID")
     
-    Month.hour <- seq(from = as.POSIXct(paste0(j,"-01 0:00"), tz = 'America/Los_Angeles'), # A list of timezone areas for reference: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-                      to = as.POSIXct(paste0(j,"-", lastday, " 24:00"), tz = 'America/Los_Angeles'),
-                      by = "hour")
+    Waze.seg.time.all <- rbind(Waze.seg.time.all, ti.Waze.seg.w)
+    Crash.seg.time.all <- rbind(Crash.seg.time.all, ti.Waze.seg.c)
     
-    # Make a segment time all table
-    SegIDTime <- expand.grid(Month.hour, SegIDall)
-    names(SegIDTime) <- c("SegDayHour", "Seg_ID")
-    
-    # Assign Waze points to the table
-    
-    # Assign Bellevue points to the table
-    
-    cat("Completed", j, "\n") # end the loop
+    i=i+3600
+    counter = counter + 1
+    if(counter %% 3600*24 == 0) cat(paste(i, "\n"))
+  } # end loop
+  
+  # Remove any duplicates and NA segment IDs. Should be none.
+  Waze.seg.time <- unique(Waze.seg.time.all) 
+  Waze.seg.time <- filter(Waze.seg.time, !is.na(RDSEG_ID))
+
+  Crash.seg.time <- unique(Crash.seg.time.all) 
+  Crash.seg.time <- filter(Crash.seg.time, !is.na(RDSEG_ID))
+  
+  #Save list of segment and time windows with crash or Waze data  
+  fn = paste0("WazeSegTimeList_", j, ".RData")
+  
+  save(list=c("Waze.seg.time", "Crash.seg.time"), file = file.path(data.loc, 'Segments', fn))
+  
+  timediff = Sys.time() - StartTime
+  
+  cat("Completed", j, 'in', round(timediff, 2), attr(timediff, 'units'), "\n") # end the loop
 }
 
 stopCluster(cl); gc()
-
-
-
-# <<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>
-# OLD TN CODE FOR REFERENCE ----
-# Delete when Bellevue code completed
-# <<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>
-OLDCODE = F
-
-if(OLDCODE){
-
-    # Make data frame of all Grid IDs by day of year and time of day in each month of data (subset to all grid IDs with Waze OR EDT data)
-    GridIDall <- unique(c(as.character(link.waze.tn$GRID_ID), # Grid ID for a Waze event
-                          as.character(link.waze.tn$GRID_ID.TN)) # Grid ID for a TN crash
-                        )
-    # x <- data.frame(length(unique(as.character(link.waze.tn$GRID_ID))), length(unique(as.character(link.waze.tn$GRID_ID.TN))), length(GridIDall), length(unique(GridIDall)))
-    # 837, 698, 1535, 922 # using "2017-04" as an example, apparently, we have some duplication in GridIDall
-    
-    # Date/time for TN crash only events or TN/Waze matching events are stored as 'date', while Date/time for Waze only events are stored as 'time'.
-    year.month.w <- format(link.waze.tn$time, "%Y-%m")
-    year.month.t <- format(link.waze.tn$date, "%Y-%m")
-    year.month <- year.month.w
-    year.month[is.na(year.month)] <- year.month.t[is.na(year.month)]
-    
-    link.waze.tn <- link.waze.tn[year.month == j,]
-    
-    month.days.w  <- unique(as.numeric(format(link.waze.tn$time, "%d"))) # Waze event date/time
-    month.days.t  <- unique(as.numeric(format(link.waze.tn$date, "%d"))) # TN crash date/time
-    month.days = unique(c(month.days.w, month.days.t))
-    
-    lastday = max(month.days[!is.na(month.days)])
-    
-    Month.hour <- seq(from = as.POSIXct(paste0(j,"-01 0:00"), tz = 'America/Chicago'), 
-                      to = as.POSIXct(paste0(j,"-", lastday, " 24:00"), tz = 'America/Chicago'), # Why timezone of from and to are different? Looks like the code only used CDT
-                      by = "hour")
-    
-    GridIDTime <- expand.grid(Month.hour, GridIDall)
-    names(GridIDTime) <- c("GridDayHour", "GRID_ID")
-  
-    # Temporally matching
-    # Match between the first reported time and last pull time of the Waze event. 
-    StartTime <- Sys.time()
-    t.min = min(Month.hour) # format(min(Month.hour), "%Y-%m-%d %H:%M:%S %Z") # the datetime format of the first hour showed as date only format. When reformat using format() function, it did not work.
-    t.max = max(Month.hour) # format(max(Month.hour), "%Y-%m-%d %H:%M:%S %Z")
-    i = t.min
-    
-    Waze.hex.time.all <- vector()
-    counter = 1
-    while(i+3600 <= t.max){
-      ti.GridIDTime = filter(GridIDTime, GridDayHour == i)
-      ti.link.waze.tn = link.waze.tn %>% filter(time >= i & time <= i+3600 | last.pull.time >= i & last.pull.time <=i+3600) # Match Waze events time
-      # table(format(link.waze.tn$time, "%Z")) # all Waze events are in EDT timezone.
-      ti.link.waze.tn.t = link.waze.tn %>% filter(date >= i & date <= i+3600) # Match TN crash time
-      # table(format(link.waze.tn$date, "%Z")) # All TN crashes are in CDT timezone. One solution is to format the original dataset by setting the local time with accurate timezone information. Any events fall in one Eastern timezone will have "EDT" in their timestamp.
-      
-      ti.Waze.hex <- inner_join(ti.GridIDTime, ti.link.waze.tn, by = "GRID_ID") # Use left_join to get zeros if no match  
-      ti.Waze.hex.t <- inner_join(ti.GridIDTime, ti.link.waze.tn.t, by = "GRID_ID") # Same, for TN only crashes
-      
-      Waze.hex.time.all <- rbind(Waze.hex.time.all, ti.Waze.hex)
-      Waze.hex.time.all <- rbind(Waze.hex.time.all, ti.Waze.hex.t)
-      
-      i=i+3600
-      if(counter %% 3600*24 == 0) cat(paste(i, "\n"))
-     } # end loop
-    
-    EndTime <- Sys.time() - StartTime
-    cat(round(EndTime, 2), attr(EndTime, "units"), "\n")
-    
-    Waze.hex.time <- unique(Waze.hex.time.all) # Rows with match = "M" are duplicated, so we want to remove the duplicates.
-    Waze.hex.time <- filter(Waze.hex.time, !is.na(GRID_ID))
-    
-    #Save list of Grid cells and time windows with EDT or Waze data  
-    fn = paste0("WazeHexTimeList_", j,"_", g, "TN.RData")
-    
-    save(list="Waze.hex.time", file = file.path(temp.outputdir, fn))
-    
-    cat("Completed", j, "\n")
-}
+sink() # Close the log file 
