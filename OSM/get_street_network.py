4cf881468147662680de8c419d117b8134330a28
Eric Englin <eric.englin@dot.gov>
2024-01-26 10:08:40 -0500

    starting osm pulls

--- OSM/get_street_network.py
+++ OSM/get_street_network.py
@@ -6,31 +6,49 @@ from shapely import Polygon, geometry, wkt
 import pandas as pd
 import osmnx as ox
 import matplotlib.pyplot as plt
+import time
+
 
 # %%
 
 # Get the state network for Seattle and Tacoma
 place_list = [
    	{
-        'city': 'Seattle',
         'state': 'Washington',
         'country': 'USA'
 }
 ]
 # %%
+start = time.process_time()
 
 ox.settings.requests_kwargs = {'verify': False}
 ox.settings.log_console = True
 
 place_network = ox.graph_from_place(place_list, network_type='drive', simplify=True)
 #fig, ax = ox.plot_graph(washington, node_size=2, node_color='r', edge_color='w', edge_linewidth=0.2)
-ox.save_graph_xml(place_network, filepath='./data/graph.osm')
+
+end = time.process_time()
+print(end - start)
+
+
 # %%
 nodes, edges = ox.utils_graph.graph_to_gdfs(place_network)
+
+# %%
 nodes.head()
 
+# %%
 edges.iloc[:, :4].head()
 
+# %%
+
+
+start = time.process_time()
+
+ox.save_graph_xml(place_network, filepath='./data/graph.osm')
+
+end = time.process_time()
+print(end - start)
 
 
 
