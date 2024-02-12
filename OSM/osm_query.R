74d28076fdca9a6ffbf462fb3372d10313cd90e6
Robin Wilkinson <robin.wilkinson@dot.gov>
2024-02-12 08:53:21 -0500

    OSM Query Script
    Script for querying the OSM api and retrieving specified roadway networks for selected states. Also included NC network shapefiles

--- OSM/osm_query.R
+++ OSM/osm_query.R
@@ -3,13 +3,17 @@ library(osmdata)
 library(sf)
 library(ggplot2)
 
+setwd(file.path(dirname(rstudioapi::getActiveDocumentContext()$path)))
+
 ##select server to query OSM api
+#If you the below doesn't work, try https://overpass.kumi.systems/api/interpreter
+
 new_url <- "https://overpass-api.de/api/interpreter"
 set_overpass_url(new_url)
 
 
 ##identify state for analysis
-state <- 'Tennessee'
+state <- 'North Carolina'
 
 #Retrieves relevant coordinates; always a rectangle
 state_bbox <- getbb(state)
@@ -26,14 +30,17 @@ map_data <- opq(bbox = state_bbox) %>%
   add_osm_feature(key = 'highway', value = i) %>%
   osmdata_sf()
 
-roadway <- c(roadway, list(map_data))
+roadways <- c(roadways, list(map_data))
 
 }
 
-total_network <- do.call(c, roadway)
+
+total_network <- do.call(c, roadways)
 
 ggplot() + geom_sf(data = total_network$osm_lines, color = 'blue')
 
 
+nc_network <- total_network$osm_lines %>%
+  select(geometry)
 
-
+write_sf(nc_network, "nc_road_network/nc_expanded_network.shp")
