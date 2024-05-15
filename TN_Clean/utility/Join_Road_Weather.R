1f101563be45464c433d2f59a315d33ff3f7e6b2
Joey Reed <112653410+jreed-volpe@users.noreply.github.com>
2024-05-15 13:47:04 -0400

    Timezone Fix
    Have to change year frequently based on the downloaded data. But assuming the daily and hourly data is there for the year, and the hourly in year + 1 (in 99% cases) is there t will run.

--- TN_Clean/utility/Join_Road_Weather.R
+++ TN_Clean/utility/Join_Road_Weather.R
@@ -5,8 +5,8 @@
 
 # Notes: 
 # Some crazy high numbers for precip and temp. 
-# Will need to consider how we are handling the timezone factor. Simply adjusting the time zone will leave a couple hours without data. 
 # There is still a lot of NAs for snow when doing it weekly
+# Need more weather data, and need to filter for one year in the current timezone 
 
 # Prep --------------------------------------------------------------------
 
@@ -21,6 +21,18 @@ library(data.table) # contains FREAD
 # Projection 
 projection <- 5070 
 
+year <- 2020 # testubg with 2021
+
+
+# Functions --------------------------------------------------------------
+
+#US_timezones <- st_read(file.path(inputdir, "Shapefiles", "TimeZone", "combined-shapefile.shp"))
+# this shapefile above doesn't have adjustments in it, just names. I suggest we download the one below of the SDC or get access to the website.
+US_timezones <- st_read("https://geo.dot.gov/server/rest/services/Hosted/Time_Zones_DS/FeatureServer/0/query?returnGeometry=true&where=1=1&outFields=*&f=geojson")
+timezone_adj <- US_timezones %>% st_transform(crs=projection) %>% 
+  mutate(adjustment = as.numeric(paste0(str_sub(utc, 1, 1), str_sub(utc, 2, 3)))) %>% 
+  select(adjustment)
+
 # Hourly Data Prep --------------------------------------------------------
 
 # Read in list of GHCN hourly stations
@@ -32,13 +44,15 @@ colnames(stations) <- c('Station_ID','Latitude','Longitude','Elevation','State',
 # Filter down to the list of stations in the state of interest
 state_stations <- stations[stations$State==state,]
 
+
+load_statehourly <- function(year_var){
 # Filter down to the list of stations for which we have hourly data in the year of interest
 available <- logical(nrow(state_stations))
 for (i in 1:nrow(state_stations)) {
   ID = state_stations[i,'Station_ID']
-  filename = paste0('GHCNh_',ID,'_',year,'.psv')
+  filename = paste0('GHCNh_',ID,'_',year_var,'.psv')
   print(filename) # check what it is checking
-  ifelse(file.exists(file.path(inputdir, "Weather", "GHCN", "Hourly", year, state, filename)
+  ifelse(file.exists(file.path(inputdir, "Weather", "GHCN", "Hourly", year_var, state, filename)
   ),
   available[i] <- TRUE,
   available[i] <- FALSE
@@ -64,8 +78,8 @@ state_hourly_hist_weather <- data.frame(Station_ID=character(),
 # Read in the data from each station file for the state and compile
 for (i in 1:nrow(hr_state_stations)){
   ID = hr_state_stations[i,'Station_ID']
-  filename = paste0('GHCNh_',ID,'_',year,'.psv')
-  df_i = read.table(file.path(inputdir, "Weather", "GHCN", "Hourly", year, state, filename),
+  filename = paste0('GHCNh_',ID,'_',year_var,'.psv')
+  df_i = read.table(file.path(inputdir, "Weather", "GHCN", "Hourly", year_var, state, filename),
                     header=TRUE,
                     sep = "|",
                     fill = TRUE
@@ -83,46 +97,86 @@ for (i in 1:nrow(hr_state_stations)){
                       'snow_depth')
   state_hourly_hist_weather <- rbind(state_hourly_hist_weather,df_i)
 }
-
-
-# Daily Data Prep ---------------------------------------------------------
-year <- 2021 # no 2019 data, so need to test with 2021
-file_list <- list.files(path = file.path(inputdir, "Weather","GHCN", "Daily", year), pattern = paste0("^", state))
-df_list <- list()
-df_list = vector("list", length = length(file_list))
-
-for(i in 1:length(file_list)){
-  df_list[[i]] <- fread(file.path(inputdir, "Weather","GHCN", "Daily", year, file_list[[i]]))
+return(state_hourly_hist_weather)
 }
-state_daily_hist_weather <- do.call(bind_rows, df_list)
 
-xy <- state_daily_hist_weather %>% 
-  mutate(Date = ymd(DATE),
-         y_day = yday(Date)) %>% 
-  dplyr::filter(y_day<=7) %>% # remove y_day argument when doing everything
-  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs=4326) %>% # these are in crs 4326, so need to load, then convert
-  st_transform(crs = st_crs(projection)) %>% 
-  select(y_day, SNOW)
-  
+state_hourly_hist_weather1 <- load_statehourly(year_var = year)
 
-# data processing 
-wx <- state_hourly_hist_weather %>% 
-  dplyr::filter(Month == 1 & Day <= 7) %>% # remove this filter when doing all of it 
-  mutate(Date = ymd_h(paste0(Year, "-", Month, "-", Day, " ", Hour)),
-         y_day = yday(Date)) %>% 
+# calculate stations timezone adjustments 
+tz_adjustments <- stations %>% filter(State == state) %>% 
+  distinct(Station_ID, .keep_all = T) %>% 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs=4326) %>% # these are in crs 4326, so need to load, then convert 
+  st_transform(crs = projection) %>% 
+  st_join(timezone_adj, join=st_intersects) %>% 
+  st_drop_geometry() %>%
+  select(Station_ID, adjustment)
+
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+if(getmode(tz_adjustments$adjustment)<0){ # if the timezone is < 0 
+  year2 = year+1
+}
+
+if(getmode(tz_adjustments$adjustment)>0){
+  year2 = year-1
+}
+
+if(getmode(tz_adjustments$adjustment)!=0){ # here in case utc = 0 which is currently impossible
+  state_hourly_hist_weather2 <- load_statehourly(year_var = year2)
+}
+
+wx <- state_hourly_hist_weather1 %>% bind_rows(state_hourly_hist_weather2) %>% 
+  left_join(tz_adjustments, by="Station_ID") %>% 
+  mutate(Date = ymd_h(paste0(Year, "-", Month, "-", Day, " ", Hour))+hours(adjustment),
+         y_day = yday(Date),
+         Hour = hour(Date),
+         Year = year(Date)) %>% 
+  filter(Year==year) %>% 
+  filter(y_day <= 5) %>% #smaller sample for testing
   group_by(y_day, Hour, Longitude, Latitude) %>% # variables to keep 
   summarise(precipitation = mean(precipitation, na.rm = TRUE), # gets rid of duplicate hour reports for a station
             temperature = mean(temperature, na.rm = TRUE),
             snow_depth = max(snow_depth)) %>% # max makes sense for depth right? 
-  mutate_all(~ifelse(is.nan(.), NA, .)) %>%
+  mutate_all(~ifelse(is.nan(.), NA, .)) %>% 
   st_as_sf(coords = c("Longitude", "Latitude"), crs=4326) %>% # these are in crs 4326, so need to load, then convert 
-  st_transform(crs = st_crs(projection))
+  st_transform(crs = projection) 
+
+# Check to make sure it's working correctly
+# ggplot() +
+#   geom_sf(data = timezone_adj) +
+#   geom_sf(data = wx)
 
 if(max(as.numeric(training_frame_rc$Hour), na.rm=TRUE)==24){ # swap from 1-24 to 0-23
   training_frame_rc <- training_frame_rc %>% rename(Hour_del = Hour) %>%
     mutate(Hour = as.numeric(Hour_del)-1) %>%
     select(-Hour_del)
 }
+
+# Daily Data Prep ---------------------------------------------------------
+year <- 2021 # need 2020 data for daily
+file_list <- list.files(path = file.path(inputdir, "Weather","GHCN", "Daily", year), pattern = paste0("^", state))
+df_list <- list()
+df_list = vector("list", length = length(file_list))
+
+for(i in 1:length(file_list)){
+  print(i)
+  df_list[[i]] <- fread(file.path(inputdir, "Weather","GHCN", "Daily", year, file_list[[i]]))
+}
+state_daily_hist_weather <- do.call(bind_rows, df_list)
+
+xy <- state_daily_hist_weather %>% 
+  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs=4326) %>% # these are in crs 4326, so need to load, then convert
+  st_transform(crs = projection) %>% 
+  mutate(Date = ymd(DATE),
+         y_day = yday(Date)) %>% 
+  dplyr::filter(y_day<=5) %>% # remove y_day argument when doing everything
+  select(y_day, SNOW)
+  
+
+
 
 n = as.numeric(length(unique(wx$y_day))) * as.numeric(length(unique(wx$Hour)))
 data_list <- list()
